57cb148836d00c4863f6f7b92ff7598673bfdfae
Eric Bishop <ericpaulbishop@gmail.com>
2009-01-04 20:10:24 +0000

    allow parsing of uci firewall remote_accept sections by gargoyle-firewall-util

--- package/gargoyle-firewall-util/files/gargoyle_firewall_util.sh
+++ package/gargoyle-firewall-util/files/gargoyle_firewall_util.sh
@@ -4,6 +4,9 @@
 source /etc/functions.sh
 include /lib/network
 
+death_mark="0xFF000000/0xFF000000"
+
+
 delete_chain_from_table()
 {
 	table=$1
@@ -30,6 +33,7 @@ delete_chain_from_table()
 	
 }
 
+
 # creates a chain in the filter table that always rejects
 # if the last byte of the connmark is FF.  This byte is
 # set in dnat table if we are redirecting port, but don't
@@ -42,17 +46,66 @@ create_death_mark_chain()
 	iptables -t filter -N death_mark
 	iptables -t filter -I INPUT 1 -j death_mark
 	iptables -t filter -I FORWARD 1 -j death_mark
-	iptables -t filter -I death_mark 1 -m connmark --mark 0xFF000000/0xFF000000 -j REJECT
+	iptables -t filter -I death_mark 1 -m connmark --mark "$death_mark" -j REJECT
 }
 
+# echo "1" if death_mark chain exists, otherwise echo "0"
 death_mark_exists()
 {
-	inp=$(iptables -t filter -L INPUT | grep "death_mark" 2>/dev/null)
-	for=$(iptables -t filter -L FORWARD | grep "death_mark" 2>/dev/null)
-	exists=0
-	if [ -n "$inp" ] && [ -n "$for" ] ; then exists=1; fi
+	local inp=$(iptables -t filter -L INPUT | grep "death_mark" 2>/dev/null)
+	local fow=$(iptables -t filter -L FORWARD | grep "death_mark" 2>/dev/null)
+	local exists=0
+	if [ -n "$inp" ] && [ -n "$fow" ] ; then exists=1; fi
 	echo $exists
 }
+
+# parse remote_accept sections in firewall config and add necessary rules
+insert_remote_accept_rules()
+{
+	local config_name="firewall"
+	local section_type="remote_accept"
+
+	#add rules for remote_accepts
+	death_mark_ports=""
+	parse_remote_accept_config()
+	{
+		vars="local_port remote_port proto zone"
+		proto="tcp"
+		zone="wan"
+		for var in $vars ; do
+			config_get $var $1 $var
+		done
+		if [ -n "$local_port" ] ; then
+			echo "LOCAL_PORT = $local_port"
+			if [ -z "$remote_port"  ] ; then
+				remote_port="$local_port"
+			fi
+			if [ "$remote_port" != "$local_port" ] ; then
+				echo iptables -t nat -A "zone_"$zone"_prerouting" -p "$proto" --dport "$remote_port" -j REDIRECT --to-ports "$local_port"
+				iptables -t nat -A "zone_"$zone"_prerouting" -p "$proto" --dport "$remote_port" -j REDIRECT --to-ports "$local_port"
+				death_mark_ports="$local_port $death_mark_ports"
+			fi
+			echo iptables -t filter -A "input_$zone" -p $proto --dport "$local_port" -j ACCEPT
+			iptables -t filter -A "input_$zone" -p $proto --dport "$local_port" -j ACCEPT
+		fi
+	}
+	config_load "$config_name"
+	config_foreach parse_remote_accept_config "$section_type"
+
+	
+	#add death marks
+	for dmp in $death_mark_ports ; do
+		echo iptables -t nat -A zone_"$zone"_prerouting -p "$proto" --dport "$dmp" -j CONNMARK --set-mark "$death_mark"
+		iptables -t nat -A zone_"$zone"_prerouting -p "$proto" --dport "$dmp" -j CONNMARK --set-mark "$death_mark"
+	done
+
+	if [ -n "$death_mark_ports" ] ; then
+		create_death_mark_chain
+	fi
+}
+
+
+
 
 # creates a chain that sets third byte of connmark to a value that denotes what l7 proto 
 # is associated with connection. This only sets the connmark, it does not save it to mark
