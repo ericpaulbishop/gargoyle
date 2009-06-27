19c999b3102ce8143358a53baa562cc93520cb51
Eric Bishop <ericpaulbishop@gmail.com>
2009-06-27 02:27:45 +0000

    Fix a couple bugs in delete_chain_from_table utility

--- package/gargoyle-firewall-util/src/delete_chain_from_table.c
+++ package/gargoyle-firewall-util/src/delete_chain_from_table.c
@@ -14,15 +14,22 @@ int main(int argc, char **argv)
 {
 	char *table = argv[1];
 	char *delete_chain = argv[2];
+	if(argc != 3)
+	{
+		printf("USAGE: %s [TABLE] [CHAIN TO DELETE]\n\n", argv[0]);
+		return 0;
+	}
 
 	char *command = dynamic_strcat(3, "iptables -t ", table, " -L --line-numbers 2>/dev/null");
 	unsigned long num_lines = 0;
 	char** table_dump = get_shell_command_output_lines(command, &num_lines);
 	free(command);
 	
+
 	unsigned long line_index;
 	char* current_chain = NULL;
 	list* delete_commands = initialize_list();
+
 
 	for(line_index=0; line_index < num_lines; line_index++)
 	{
@@ -33,12 +40,13 @@ int main(int argc, char **argv)
 		if(strcmp(line_pieces[0], "Chain") == 0)
 		{
 			if(current_chain != NULL) { free(current_chain); }
-			current_chain = strdup(line_pieces[0]);
+			current_chain = strdup(line_pieces[1]);
 		}
 		else 
 		{
 			unsigned long line_num;
 			int read = sscanf(line_pieces[0], "%ld", &line_num);
+
 			if(read > 0 && current_chain != NULL && num_pieces >1)
 			{
 				if(strcmp(line_pieces[1], delete_chain) == 0)
@@ -55,8 +63,8 @@ int main(int argc, char **argv)
 	free_null_terminated_string_array(table_dump);
 	
 	/* final two commands to flush chain being deleted and whack it */
-	unshift_list(delete_commands, dynamic_strcat(4, "iptables -t ", table, " -F ", delete_chain, " 2>/dev/null"));
-	unshift_list(delete_commands, dynamic_strcat(4, "iptables -t ", table, " -X ", delete_chain, " 2>/dev/null"));
+	unshift_list(delete_commands, dynamic_strcat(5, "iptables -t ", table, " -F ", delete_chain, " 2>/dev/null"));
+	unshift_list(delete_commands, dynamic_strcat(5, "iptables -t ", table, " -X ", delete_chain, " 2>/dev/null"));
 
 	/* run delete commands */
 	while(delete_commands->length > 0)
