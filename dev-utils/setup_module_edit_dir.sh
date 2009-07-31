12ca7ec7e7e71710079619dbcd8f85c0e3dc5cd5
Eric Bishop <ericpaulbishop@gmail.com>
2009-07-31 21:33:58 +0000

    fix minor issue in setup_module_edit_dir.sh script

--- dev-utils/setup_module_edit_dir.sh
+++ dev-utils/setup_module_edit_dir.sh
@@ -23,7 +23,7 @@ cd "$module_name"_edit
 module=$(ls -d $src_dir/build*/lin*/lin*/net/ipv4/netfilter/*$module_name.c 2>/dev/null)
 deps=$(ls -d $src_dir/build*/lin*/lin*/net/ipv4/netfilter/$module_name"_deps" 2>/dev/null)
 header=$(ls -d $src_dir/build*/lin*/lin*/include/linux/netfilter_ipv4/*$module_name.h 2>/dev/null)
-extension=$(ls -d $src_dir/build*/lin*/iptables*/exten*/*module_name.c 2>/dev/null)
+extension=$(ls -d $src_dir/build*/lin*/iptables*/exten*/*$module_name.c 2>/dev/null)
 
 for link in $module $deps $header $extension ; do
 	ln -s $link 
