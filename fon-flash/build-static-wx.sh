43f26454902c1d4728717a52b97b503b42faa04a
Eric Bishop <ericpaulbishop@gmail.com>
2009-12-26 23:05:06 +0000

    fix fon-flash compile for OSX 10.6

--- fon-flash/build-static-wx.sh
+++ fon-flash/build-static-wx.sh
@@ -1,10 +1,16 @@
+osname=$(uname)
+macflags=""
+if [ "$osname" = "Darwin" ] ; then
+	macflags="CC=gcc-4.0 CXX=g++-4.0 LD=g++-4.0"
+fi
+
 #sudo apt-get install libgtk2.0-dev libgnome2-dev
 mkdir static-wx
 cd static-wx
 wget "http://downloads.sourceforge.net/wxwindows/wxWidgets-2.8.9.tar.gz"
 tar xvzf wxWidgets-2.8.9.tar.gz
 cd wxWidgets-2.8.9
-./configure --enable-optimise --enable-stl --enable-unicode --enable-threads --enable-static --disable-shared --enable-monolithic
+./configure $macflags --enable-optimise --enable-stl --enable-unicode --enable-threads --enable-static --disable-shared --enable-monolithic
 make
 cd ..
 cd ..
