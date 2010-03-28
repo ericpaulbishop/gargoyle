ee9958db67563c0147ef00a85e2204f685935c2a
Eric Bishop <ericpaulbishop@gmail.com>
2010-03-28 07:22:26 +0000

    add copyright header to restore_quotas.c in gargoyle firewall utils (somehow never got around to it before now)

--- package/gargoyle-firewall-util/src/restore_quotas.c
+++ package/gargoyle-firewall-util/src/restore_quotas.c
@@ -1,3 +1,24 @@
+/*  restore_quotas --	Used to initialize and restore bandwidth quotas based on UCI config files 
+ *  			and any previously saved quota data in /usr/data/quotas
+ *  			Originally designed for use with Gargoyle router firmware (gargoyle-router.com)
+ *
+ *
+ *  Copyright © 2009-2010 by Eric Bishop <eric@gargoyle-router.com>
+ *
+ *  This file is free software: you may copy, redistribute and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation, either version 2 of the License, or (at your
+ *  option) any later version.
+ *
+ *  This file is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
