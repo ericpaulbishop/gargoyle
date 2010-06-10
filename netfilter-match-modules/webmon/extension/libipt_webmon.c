c94cbfbc9ad59da84811f65113a00e40b47ed05f
Eric Bishop <ericpaulbishop@gmail.com>
2010-06-10 09:28:23 -0400

    fix header and extension for webmon iptables module so they reflect a working skeleton instead of weburl code

--- netfilter-match-modules/webmon/extension/libipt_webmon.c
+++ netfilter-match-modules/webmon/extension/libipt_webmon.c
@@ -1,4 +1,4 @@
-/*  weburl --	An iptables extension to match URLs in HTTP requests 
+/*  webmon --	An iptables extension to match URLs in HTTP requests 
  *  		This module can match using string match or regular expressions
  *  		Originally designed for use with Gargoyle router firmware (gargoyle-router.com)
  *
@@ -33,7 +33,7 @@
  * in pre-processor directives below
  */
 #include <iptables.h>  
-#include <linux/netfilter_ipv4/ipt_weburl.h>
+#include <linux/netfilter_ipv4/ipt_webmon.h>
 
 #ifdef _XTABLES_H
 	#define iptables_rule_match	xtables_rule_match
@@ -62,16 +62,11 @@ static void param_problem_exit_error(char* msg);
 /* Function which prints out usage message. */
 static void help(void)
 {
-	printf(	"weburl options:\n  --contains [!] [STRING]\n  --contains_regex [!] [REGEX]\n --matches_exactly [!] [STRING]\n --domain_only\n --path_only\n");
+	printf(	"webmon options:\n");
 }
 
 static struct option opts[] = 
 {
-	{ .name = "contains", 		.has_arg = 1, .flag = 0, .val = WEBURL_CONTAINS_TYPE },	//string
-	{ .name = "contains_regex", 	.has_arg = 1, .flag = 0, .val = WEBURL_REGEX_TYPE },	//regex
-	{ .name = "matches_exactly",	.has_arg = 1, .flag = 0, .val = WEBURL_EXACT_TYPE },	//exact string match
-	{ .name = "domain_only",	.has_arg = 0, .flag = 0, .val = WEBURL_DOMAIN_PART },	//only match domain portion of url
-	{ .name = "path_only",		.has_arg = 0, .flag = 0, .val = WEBURL_PATH_PART },	//only match path portion of url
 	{ .name = 0 }
 };
 
@@ -91,115 +86,25 @@ static int parse(	int c,
 			struct ipt_entry_match **match
 			)
 {
-	struct ipt_weburl_info *info = (struct ipt_weburl_info *)(*match)->data;
+	struct ipt_webmon_info *info = (struct ipt_webmon_info *)(*match)->data;
 	int valid_arg = 0;
 
-	if(*flags < 10)
-	{
-		info->match_part = WEBURL_ALL_PART;
-	}
-
-	switch (c)
-	{
-		case WEBURL_CONTAINS_TYPE:
-		case WEBURL_REGEX_TYPE:
-		case WEBURL_EXACT_TYPE:
-			info->match_type = c;
-
-			//test whether to invert rule
-			my_check_inverse(optarg, &invert, &optind, 0);
-			info->invert = invert ? 1 : 0;
-	
-			//test that test string is reasonable length, then to info
-			int testlen = strlen(argv[optind-1]);
-			if(testlen > 0 && testlen < MAX_TEST_STR)
-			{
-				strcpy(info->test_str, argv[optind-1]);
-			}
-			else if(testlen >= MAX_TEST_STR)
-			{
-				char err[100];
-				sprintf(err, "Parameter definition is too long, must be less than %d characters", MAX_TEST_STR);
-				param_problem_exit_error(err);
-			}
-			else
-			{
-				param_problem_exit_error("Parameter definition is incomplete");
-			}
-
-			if(*flags % 10 == 1)
-			{
-				param_problem_exit_error("You may only specify one string/pattern to match");
-			}
-			*flags = *flags + 1;
-			
-			valid_arg = 1;
-			break;
-
-		case WEBURL_DOMAIN_PART:
-		case WEBURL_PATH_PART:
-			info->match_part = c;
-			if(*flags >= 10)
-			{
-				param_problem_exit_error("You may specify at most one part of the url to match:\n\t--domain_only, --path_only or neither (to match full url)\n");
-			}
-			*flags = *flags+10;
-			
-			valid_arg = 1;
-			break;
-	}
-	
+		
 	return valid_arg;
+
 }
 
 
 	
-static void print_weburl_args(	struct ipt_weburl_info* info )
+static void print_webmon_args(	struct ipt_webmon_info* info )
 {
-	//invert
-	if(info->invert > 0)
-	{
-		printf("! ");
-	}
-	//match type
-	switch (info->match_type)
-	{
-		case WEBURL_CONTAINS_TYPE:
-			printf("--contains ");
-			break;
-		case WEBURL_REGEX_TYPE:
-			printf("--contains_regex ");
-			break;
-		case WEBURL_EXACT_TYPE:
-			printf("--matches_exactly ");
-			break;
-	}
-	//test string
-	printf("%s ", info->test_str);
-
-	//match part
-	switch(info->match_part)
-	{
-		case WEBURL_DOMAIN_PART:
-			printf("--domain_only ");
-			break;
-		case WEBURL_PATH_PART:
-			printf("--path_only ");
-			break;
-		case WEBURL_ALL_PART:
-			//print nothing
-			break;
-	}
 	
 }
 
 /* Final check; must have specified a test string with either --contains or --contains_regex. */
 static void final_check(unsigned int flags)
 {
-	if (flags %10 == 0)
-	{
-		param_problem_exit_error("You must specify '--contains' or '--contains_regex' or '--matches_exactly'");
-	}
+
 }
 
 /* Prints out the matchinfo. */
@@ -209,10 +114,10 @@ static void print(const void *ip, const struct xt_entry_match *match, int numeri
 static void print(const struct ipt_ip *ip, const struct ipt_entry_match *match, int numeric)
 #endif
 {
-	printf("WEBURL ");
-	struct ipt_weburl_info *info = (struct ipt_weburl_info *)match->data;
+	printf("WEBMON ");
+	struct ipt_webmon_info *info = (struct ipt_webmon_info *)match->data;
 
-	print_weburl_args(info);
+	print_webmon_args(info);
 }
 
 /* Saves the union ipt_matchinfo in parsable form to stdout. */
@@ -222,21 +127,21 @@ static void save(const void *ip, const struct xt_entry_match *match)
 static void save(const struct ipt_ip *ip, const struct ipt_entry_match *match)
 #endif
 {
-	struct ipt_weburl_info *info = (struct ipt_weburl_info *)match->data;
-	print_weburl_args(info);
+	struct ipt_webmon_info *info = (struct ipt_webmon_info *)match->data;
+	print_webmon_args(info);
 }
 
-static struct iptables_match weburl = 
+static struct iptables_match webmon = 
 { 
 	.next		= NULL,
- 	.name		= "weburl",
+ 	.name		= "webmon",
 	#ifdef XTABLES_VERSION_CODE
 		.version = XTABLES_VERSION,
 	#else
 		.version = IPTABLES_VERSION,
 	#endif
-	.size		= IPT_ALIGN(sizeof(struct ipt_weburl_info)),
-	.userspacesize	= IPT_ALIGN(sizeof(struct ipt_weburl_info)),
+	.size		= IPT_ALIGN(sizeof(struct ipt_webmon_info)),
+	.userspacesize	= IPT_ALIGN(sizeof(struct ipt_webmon_info)),
 	.help		= &help,
 	.parse		= &parse,
 	.final_check	= &final_check,
@@ -247,7 +152,7 @@ static struct iptables_match weburl =
 
 void _init(void)
 {
-	register_match(&weburl);
+	register_match(&webmon);
 }
 
 
@@ -257,27 +162,8 @@ void _init(void)
 #ifndef FALSE
 #define FALSE 0
 #endif
-static int  my_check_inverse(const char option[], int* invert, int *my_optind, int argc)
-{
-	if (option && strcmp(option, "!") == 0)
-	{
-		if (*invert)
-		{
-			param_problem_exit_error("Multiple `!' flags not allowed");
-		}
-		*invert = TRUE;
-		if (my_optind != NULL)
-		{
-			++*my_optind;
-			if (argc && *my_optind > argc)
-			{
-				param_problem_exit_error("no argument following `!'");
-			}
-		}
-		return TRUE;
-	}
-	return FALSE;
-}
+
+
 static void param_problem_exit_error(char* msg)
 {
 	#ifdef xtables_error
