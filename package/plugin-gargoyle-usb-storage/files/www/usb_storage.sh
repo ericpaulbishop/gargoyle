3b2a8397caef1622c54607bb17686b917a099717
Eric <ericpaulbishop@gmail.com>
2010-10-28 18:08:31 -0400

    make sure usb_storage plugin displays properly in Gargoyle when it is installed

--- package/plugin-gargoyle-usb-storage/files/www/usb_storage.sh
+++ package/plugin-gargoyle-usb-storage/files/www/usb_storage.sh
@@ -6,7 +6,7 @@
 	# itself remain covered by the GPL. 
 	# See http://gargoyle-router.com/faq.html#qfoss for more information
 	eval $( gargoyle_session_validator -c "$COOKIE_hash" -e "$COOKIE_exp" -a "$HTTP_USER_AGENT" -i "$REMOTE_ADDR" -r "login.sh" -t $(uci get gargoyle.global.session_timeout) -b "$COOKIE_browser_time"  )
-	gargoyle_header_footer -h -s "firewall" -p "quotas" -c "internal.css" -j "table.js usb_storage.js" gargoyle nfsd samba
+	gargoyle_header_footer -h -s "system" -p "usb_storage" -c "internal.css" -j "table.js usb_storage.js" gargoyle nfsd samba
 
 ?>
 
@@ -108,7 +108,7 @@
 
 
 <?
-	gargoyle_header_footer -f -s "firewall" -p "usb_storage"
+	gargoyle_header_footer -f -s "system" -p "usb_storage"
 ?>
 
 
