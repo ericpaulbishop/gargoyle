11937552570dec8d15098496d2aa6017a7f241d0
Eric <ericpaulbishop@gmail.com>
2010-11-01 20:33:05 -0400

    fix a few issues in do_release script

--- dev-utils/do_release.sh
+++ dev-utils/do_release.sh
@@ -3,20 +3,22 @@
 user=$1
 if [ -z "$user" ] ; then
 	echo "Error: must specify user as first argument"
+	exit
 fi
 
 
-SCP=scp -o StrictHostKeyChecking=no -o PubkeyAuthentication=yes -o BatchMode=yes
-SSH=ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=yes -o BatchMode=yes
+alias SCP='scp -o StrictHostKeyChecking=no -o PubkeyAuthentication=yes -o BatchMode=yes'
+alias SSH='ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=yes -o BatchMode=yes'
 
 
 #upload images and packages
 cd images
 image_dirs=$(ls)
 for i in $image_dirs ; do
-	$SCP $i/* $user@gargoyle-router.com:gargoyle_site/downloads/images/$i/
-	$SSH $user@gargoyle-router.com "mkdir -p gargoyle_site/packages/backfire/$i"
-	$SCP ../built/$i/* $user@gargoyle-router.com:gargoyle_site/packages/backfire/$i/
+	echo $i
+	SCP $i/* $user@gargoyle-router.com:gargoyle_site/downloads/images/$i/
+	SSH $user@gargoyle-router.com "mkdir -p gargoyle_site/packages/backfire/$i"
+	SCP ../built/$i/* $user@gargoyle-router.com:gargoyle_site/packages/backfire/$i/
 done
 
 #upload latest code
@@ -26,10 +28,10 @@ cd src
 git clone git://gargoyle-router.com/gargoyle.git
 tar cvzf gargoyle_$version-src.tar.gz gargoyle
 rm -rf gargoyle
-$SCP gargoyle_$version-src.tar.gz $user@gargoyle-router.com:gargoyle_site/downloads/src/
+SCP gargoyle_$version-src.tar.gz $user@gargoyle-router.com:gargoyle_site/downloads/src/
 
 #update download list
-$SSH $user@gargoyle-router.com  "./update.sh"
+SSH $user@gargoyle-router.com  "./update.sh"
 
 
 #tag release
