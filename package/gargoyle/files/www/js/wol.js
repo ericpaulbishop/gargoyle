478034c251269912b4370f053b98142322c7a74b
Eric <ericpaulbishop@gmail.com>
2011-01-23 23:57:59 -0500

    add separate wake on lan section

---
+++ package/gargoyle/files/www/js/wol.js
@@ -0,0 +1,100 @@
+
+
+
+function initWolTable()
+{
+	var dataList = [];
+	var ipToHostAndMac = [];
+	
+
+	arpLines.shift(); //skip header
+	var lineIndex = 0;
+	for(lineIndex=0; lineIndex < arpLines.length; lineIndex++)
+	{
+		var nextLine = arpLines[lineIndex];
+		var splitLine = nextLine.split(/[\t ]+/);
+		var mac = splitLine[3].toUpperCase();
+		var ip = splitLine[0];
+		dataList.push( [ getHostname(ip), ip, mac, createWakeUpButton() ] );
+		ipToHostAndMac[ip] = 1;
+	}
+
+	for(lineIndex=0; lineIndex < dhcpLeaseLines.length; lineIndex++)
+	{
+		var leaseLine = dhcpLeaseLines[lineIndex];
+		var splitLease = leaseLine.split(/[\t ]+/);
+		var mac = splitLease[1].toUpperCase();
+		var ip = splitLease[2];
+		if(ipToHostAndMac[ip] == null)
+		{
+			dataList.push( [ getHostname(ip), ip, mac, createWakeUpButton() ] );
+			ipToHostAndMac[ip] = 1;
+		}
+	}
+
+	for(lineIndex=0; lineIndex < etherData.length; lineIndex++)
+	{
+		var ether = etherData[lineIndex];
+		var mac = ether[0].toUpperCase();
+		var ip = ether[1];
+		if(ipToHostAndMac[ip] == null)
+		{
+			dataList.push( [ getHostname(ip), ip, mac, createWakeUpButton() ] );
+			ipToHostAndMac[ip] = 1;
+		}
+	}
+
+
+	sort2dStrArr(dataList, 1);
+	var columnNames = ["Hostname", "IP", "MAC", "" ]
+	var table = createTable(columnNames, dataList, "wol_table", false, false);
+	var tableContainer = document.getElementById('wol_table_container');
+	if(tableContainer.firstChild != null)
+	{
+		tableContainer.removeChild(tableContainer.firstChild);
+	}
+	tableContainer.appendChild(table);
+}
+
+
+function sort2dStrArr(arr, testIndex)
+{
+	var str2dSort = function(a,b){  return a[testIndex] == b[testIndex] ? 0 : (a[testIndex] < b[testIndex] ? -1 : 1);  }
+	arr.sort(str2dSort);
+}
+
+
+function getHostname(ip)
+{
+	var hostname = ipToHostname[ip] == null ? "(unknown)" : ipToHostname[ip];
+	hostname = hostname.length < 25 ? hostname : hostname.substr(0,22)+"...";
+	return hostname;
+}
+
+
+function createWakeUpButton()
+{
+        var WakeUpButton = createInput("button");
+        WakeUpButton.value = "WakeUp";
+        WakeUpButton.className="default_button";
+        WakeUpButton.onclick = wakeHost;
+        return WakeUpButton;
+}
+
+function wakeHost() {
+
+	getRow=this.parentNode.parentNode;
+
+	var mac = getRow.childNodes[1].firstChild.data;
+	var wakeHostCommand = [ "wol -i " + bcastIp + " " + mac ];
+
+	commands = wakeHostCommand.join("\n");
+	var param = getParameterDefinition("commands", commands) + "&" + getParameterDefinition("hash", document.cookie.replace(/^.*hash=/,"").replace(/[\t ;]+.*$/, ""));
+	runAjax("POST", "utility/run_commands.sh", param, function(){ return 0; });
+
+        alert("Wakeup request sent");
+
+}
+
+
+
---
+++ package/gargoyle/files/www/wol.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/haserl
+<?
+	# This program is copyright © 2008-2011 Eric Bishop and is distributed under the terms of the GNU GPL 
+	# version 2.0 with a special clarification/exception that permits adapting the program to 
+	# configure proprietary "back end" software provided that all modifications to the web interface
+	# itself remain covered by the GPL. 
+	# See http://gargoyle-router.com/faq.html#qfoss for more information
+	eval $( gargoyle_session_validator -c "$COOKIE_hash" -e "$COOKIE_exp" -a "$HTTP_USER_AGENT" -i "$REMOTE_ADDR" -r "login.sh" -t $(uci get gargoyle.global.session_timeout) -b "$COOKIE_browser_time"  )
+	gargoyle_header_footer -h -s "status" -p "wol" -c "internal.css" -j "wol.js table.js" -i -n dhcp wireless
+?>
+
+<script>
+<!--
+var dhcpLeaseLines;
+var wifiLines;
+var wirelessDriver;
+var conntrackLines;
+var arpLines;
+<?
+	sh /usr/lib/gargoyle/define_host_vars.sh
+	echo "var etherData = new Array();";
+	if [ -e /etc/ethers ] ; then
+		cat /etc/ethers | awk ' $0 ~ /^[\t ]*[0-9abcdefABCDEF]/ {print "etherData.push([\""$1"\",\""$2"\"]);"};'
+	fi
+	wan_ip=$(uci -p /tmp/state get network.wan.ipaddr 2>/dev/null)
+	lan_ip=$(uci -p /tmp/state get network.lan.ipaddr 2>/dev/null)
+	echo "currentWanIp=\"$wan_ip\";"
+	echo "currentLanIp=\"$lan_ip\";"
+	echo "var bcastIp=\"$(ifconfig | awk '/^br-lan/{s=$1;getline;print $3}' | cut -b7-)\";"
+?>
+//-->
+</script>
+
+<fieldset id="wol_data">
+	<legend class="sectionHeader">Wake on LAN</legend>
+	<div id="wol_table_container"></div>
+</fieldset>
+
+
+<script>
+<!--
+	initWolTable();
+//-->
+</script>
+
+<?
+	gargoyle_header_footer -f -s "connection" -p "wol"
+?>
