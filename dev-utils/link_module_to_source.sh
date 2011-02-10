8948a3d2ada822c32ac866d3010287da8d7c5189
Eric <ericpaulbishop@gmail.com>
2011-02-10 15:54:55 -0500

    fix link_module_to_source script

--- dev-utils/link_module_to_source.sh
+++ dev-utils/link_module_to_source.sh
@@ -22,20 +22,22 @@ deps=$(ls -d $src_dir/build*/lin*/lin*/net/ipv4/netfilter/$module_name"_deps" 2>
 header=$(ls -d $src_dir/build*/lin*/lin*/include/linux/netfilter_ipv4/*$module_name.h 2>/dev/null)
 extension=$(ls -d $src_dir/build*/lin*/iptables*/exten*/*$module_name.c 2>/dev/null)
 
+net_dir="$current_dir/netfilter-match-modules"
+
 if [ -n "$module" ] ; then
 	rm -rf "$module"
-	ln -s $current_dir/$module_name/module/ipt_$module_name.c "$module"
+	ln -s $net_dir/$module_name/module/ipt_$module_name.c "$module"
 fi
 if [ -n "$deps" ] ; then
 	rm -rf "$deps"
-	ln -s "$current_dir/$module_name/module/$module_name"_deps "$deps"
+	ln -s "$net_dir/$module_name/module/$module_name"_deps "$deps"
 fi
 if [ -n "$header" ] ; then
 	rm -rf "$header"
-	ln -s $current_dir/$module_name/header/ipt_$module_name.h "$header"
+	ln -s $net_dir/$module_name/header/ipt_$module_name.h "$header"
 fi
 if [ -n "$extension" ] ; then
 	rm -rf "$extension"
-	ln -s $current_dir/$module_name/extension/libipt_$module_name.c "$extension"
+	ln -s $net_dir/$module_name/extension/libipt_$module_name.c "$extension"
 fi
 
