d3068d03610d4e180d0f90f7d193a1a766f5fcd8
Eric <ericpaulbishop@gmail.com>
2011-03-12 15:51:03 -0500

    fix bugs in retest.sh script

--- dev-utils/retest.sh
+++ dev-utils/retest.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 firmware_file="openwrt-ar71xx-tl-wr1043nd-v1-squashfs-sysupgrade.bin"
-firmware=openwrt_build/bin/ar71xx/$firmware_file
-
+firmware=bin/ar71xx/$firmware_file
+router_ip="192.168.1.1"
+pass="password"
+user="root"
 
 cd build*/linux*/iptable*
 find . -name "*.*o*" | xargs rm -rf
@@ -13,17 +15,25 @@ cd ../../../../../..
 
 make V=99
 
-empty -f -i my_in -o my_out scp -o StrictHostKeyChecking=no  "$firmware" "$user@$router_ip:/tmp"
-empty -w -i my_out -o my_in "word: " "$pass\n"
+my_in=/tmp/my_in
+my_out=/tmp/my_out
+rm -rf $my_in $my_out
+
+
+empty -f -i $my_in  -o $my_out /usr/bin/scp -o StrictHostKeyChecking=no  "$firmware" "$user@$router_ip:/tmp"
+empty -w -i $my_out -o $my_in "word: " "$pass\n"
 sleep 3
 empty -k >/dev/null 2>&1
+killall empty >/dev/null 2>&1
 
 
-empty -f -i my_in -o my_out ssh -o StrictHostKeyChecking=no  $user@$router_ip
-empty -w -i my_out -o my_in "word: " "$pass\n"
+empty -f -i $my_in  -o $my_out /usr/bin/ssh -o StrictHostKeyChecking=no  $user@$router_ip
+empty -w -i $my_out -o $my_in "word: " "$pass\n"
 
 
-empty -s -o my_in "sysupgrade -n /tmp/$firmware_file\n"
-empty -w -i my_out -o my_in "boot " "\n"
+empty -s -o $my_in "sysupgrade -n /tmp/$firmware_file\n"
+empty -w -i $my_out -o $my_in "boot " "\n" >/dev/null 2>&1 
+empty -k >/dev/null 2>&1
+killall empty >/dev/null 2>&1
 
-
+rm -rf $my_in $my_out
