2ec671c629945f07f05cf6b5a848da75cbddf10c
Eric <ericpaulbishop@gmail.com>
2011-03-22 18:37:59 -0400

    update to latest backfire revision

--- full-build.sh
+++ full-build.sh
@@ -27,7 +27,7 @@ adj_num_version=$(echo "$version_name" | sed 's/X/0/g' | sed 's/x/0/g' | sed 's/
 # set svn revision number to use 
 # you can set this to an alternate revision 
 # or empty to checkout latest 
-rnum=24714
+rnum=26252
 
 
 #download openwrt source if we haven't already
