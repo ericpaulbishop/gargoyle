efa82178b30d6a30af948f587c43ca77704bf311
Eric <ericpaulbishop@gmail.com>
2011-06-25 12:04:57 -0400

    add gcc-multilib to debian build dependencies script

--- dev-utils/debian_install_build_deps.sh
+++ dev-utils/debian_install_build_deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-apt-get install vim g++ build-essential subversion git-core wget curl bison automake flex libncurses5-dev gawk
+apt-get install vim g++ build-essential subversion git-core wget curl bison automake flex libncurses5-dev gawk gcc-multilib
