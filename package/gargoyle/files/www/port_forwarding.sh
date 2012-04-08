62b95997828f354ffac3d658862e246bc38dbb71
Paul Bixel <bixelps@yahoo.com>
2012-04-08 19:37:08 -0400

    Enhance Plug and Play GUI, add help and forward table.

--- package/gargoyle/files/www/port_forwarding.sh
+++ package/gargoyle/files/www/port_forwarding.sh
@@ -6,7 +6,7 @@
 	# itself remain covered by the GPL. 
 	# See http://gargoyle-router.com/faq.html#qfoss for more information
 	eval $( gargoyle_session_validator -c "$COOKIE_hash" -e "$COOKIE_exp" -a "$HTTP_USER_AGENT" -i "$REMOTE_ADDR" -r "login.sh" -t $(uci get gargoyle.global.session_timeout) -b "$COOKIE_browser_time"  )	
-	gargoyle_header_footer -h -s "firewall" -p "portforwarding" -c "internal.css" -j "port_forwarding.js  table.js" -i firewall network dropbear upnpd
+	gargoyle_header_footer -h -s "firewall" -p "portforwarding" -c "internal.css" -j "port_forwarding.js  table.js" gargoyle -i firewall network dropbear upnpd
 ?>
 
 <script>
@@ -18,7 +18,6 @@
 	else
 		echo "var upnpdEnabled = false;"
 	fi
-
 
 ?>
 //-->
@@ -75,18 +74,22 @@
 		<legend class="sectionheader">UPnP / NAT-PMP</legend>
 		<div id='upnp_enabled_container'>
 			<input type='checkbox' id='upnp_enabled' onclick="setUpnpEnabled()" />
-			<label id='upnp_enabled_label' for='upnp_enabled'>UPnP &amp; NAT-PMP Enabled</label>
-		</div>
-		<div class='indent'>
-			<em>WARNING: The use of UPnP / NAT-PMP makes your system vulnerable to certain forms of attack, and is therefore
-			a security risk.  However, UPnP is (unfortunately) necessary for some Microsoft applications including MSN messenger and
-			the risk is relatively small.  Nevertheless, because of this security issue, UPnP is disabled by default.
-			It is recommended that you enable UPnP only if you are sure you need it.</em>
+			<label id='upnp_enabled_label' for='upnp_enabled'>Enable UPnP &amp; NAT-PMP service</label>
 		</div>
 
+
+		<div id='upnp_table_heading_container'>
+			<span class='nocolumn'>Active port forwards:</span>
+		</div>
+ 
+		<br>
+
+		<div class='indent'>
+			<div id='upnp_table_container' class="bottom_gap"></div>
+		</div>
 
 		<div id='upnp_up_container'>
-			<label class='leftcolumn' for='upnp_up' id='upnp_up_label'>Max Up Speed on UPnP Ports:</label>
+			<label class='leftcolumn' for='upnp_up' id='upnp_up_label'>Upload speed to report:</label>
 			<span class = 'rightcolumn'>
 				<input type='text' class='rightcolumn' id='upnp_up' onkeyup='proofreadNumeric(this)' size='5' maxlength='5' />
 				<em>Kbit / second</em>
@@ -94,13 +97,37 @@
 		</div>
 
 		<div id='upnp_down_container'>
-			<label class='leftcolumn' for='upnp_down' id='upnp_down_label'>Max Down Speed on UPnP Ports:</label>
+			<label class='leftcolumn' for='upnp_down' id='upnp_down_label'>Download speed to report:</label>
 			<span class='rightcolumn'>
 				<input type='text' id='upnp_down' onkeyup='proofreadNumeric(this)' size='5' maxlength='5' />
 				<em>Kbit / second</em>
 			</span>
+
+
+		<div id="upnp_help" class="indent">
+		<span id='upnp_help_txt'>
+
+		<p>UPnP (Univeral Plug and Play) and NAT-PMP (NAT Port Mapping Protocol) are both protocols which allows devices and applications on the 
+		LAN to automatically configure your router with the port forwards needed for proper operation.  If a device supports either protocol
+		it is not necessary to create manual port forward rules at the top of this page as they will automatically be created by the device.</p>
+
+		<p>When enabled Gargoyle shows a table of automatically created port forwards so you can see which devices have requested forwards and
+		ensure that this feature is working properly.  This service may not work correctly in network configurations containing two more 
+		routers (double NAT).  If you see a single row with '***' in it it means there are no port forwards registered.</p>
+
+		<p>As part of the protocal the LAN device and request the speed of the WAN connection from the router.  Two fields are provided to configure
+		the response to such quiries. Client applications can use this information to optimise their performance.  But is important to note that 
+		the router does not do anything to limit speeds based on this data.  It is only reported to the requester.  If zero is entered for either 
+		value the speed of the interface is reported, usually 100MB or 1GB depending on the routers interface speed.</p>
+
+		<p>There is some contraversy about the security of this service and it does require additional RAM to run which may be important on memory
+		constrainted routers, so by default is left off.</p>
+
+		</span>
+		<a id="upnp_help_ref" onclick='setDescriptionVisibility("upnp_help")'  href="#upnp_help">Hide Text</a>
+
 		</div>
-		
+
 	</fieldset>
 	
 	
