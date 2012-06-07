cec252c4d5628be8c67c7657dc06e988c04fd32d
Eric Bishop <ericpaulbishop@gmail.com>
2012-06-07 10:52:43 -0400

    set upload and tmp dir for haserl call in openvpn client upload script

--- package/plugin-gargoyle-openvpn/files/www/utility/openvpn_upload_client.sh
+++ package/plugin-gargoyle-openvpn/files/www/utility/openvpn_upload_client.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/haserl
+#!/usr/bin/haserl --upload-limit=1048576 --upload-dir=/tmp/
 <?
 
 eval $( gargoyle_session_validator -c "$POST_hash" -e "$COOKIE_exp" -a "$HTTP_USER_AGENT" -i "$REMOTE_ADDR" -r "login.sh" -t $(uci get gargoyle.global.session_timeout) -b "$COOKIE_browser_time"  )	
