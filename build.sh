69d3443a4b1c22a0b9d3e9479ebd60b309288d10
Eric <ericpaulbishop@gmail.com>
2012-09-16 12:56:15 -0400

    bump openwrt to latest

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ set_version_variables()
 	# set svn revision number to use 
 	# you can set this to an alternate revision 
 	# or empty to checkout latest 
-	rnum=33378
+	rnum=33444
 
 	#set date here, so it's guaranteed the same for all images
 	#even though build can take several hours
