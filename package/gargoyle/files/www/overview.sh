2f236efb733965eaf536cb514c069fc9d4062f55
Eric <ericpaulbishop@gmail.com>
2012-10-20 09:50:07 -0400

    lan ip shouldn't be included in wan dns servers

--- package/gargoyle/files/www/overview.sh
+++ package/gargoyle/files/www/overview.sh
@@ -79,7 +79,12 @@
 		echo "var model=\"Unknown\";"
 	fi
 
-	echo "var wanDns=\""$(sed -e '/nameserver/!d; s#nameserver ##g' /tmp/resolv.conf.auto | sort | uniq)"\";"
+	lan_ip=$(sbin/uci get network.lan.ipaddr 2>/dev/null)
+	if [ -n "$lan_ip" ] ; then
+		echo "var wanDns=\""$(sed -e '/nameserver/!d; s#nameserver ##g' /tmp/resolv.conf.auto | sort | uniq | grep -v "$lan_ip" )"\";"
+	else
+		echo "var wanDns=\""$(sed -e '/nameserver/!d; s#nameserver ##g' /tmp/resolv.conf.auto | sort | uniq )"\";"
+	fi
 
 ?>
 //-->
