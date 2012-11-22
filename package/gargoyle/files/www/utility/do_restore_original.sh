5230eacdc7a6b5b2c8b71566f66bf91a0a85410e
Eric <ericpaulbishop@gmail.com>
2012-11-21 20:21:17 -0500

    switch to doing restore via firstboot command in reset button handler too

--- package/gargoyle/files/www/utility/do_restore_original.sh
+++ package/gargoyle/files/www/utility/do_restore_original.sh
@@ -7,6 +7,6 @@
 	# See http://gargoyle-router.com/faq.html#qfoss for more information
 	eval $( gargoyle_session_validator -c "$POST_hash" -e "$COOKIE_exp" -a "$HTTP_USER_AGENT" -i "$REMOTE_ADDR" -r "login.sh" -t $(uci get gargoyle.global.session_timeout) -b "$COOKIE_browser_time"  )	
 
-	firstboot >/dev/null 2>&1
+	/sbin/firstboot >/dev/null 2>&1
 	
 ?>
