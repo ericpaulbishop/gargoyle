24ded267b97407d5e2ff4089e826efa719c8eeda
Cezary Jackiewicz <cezary.jackiewicz@gmail.com>
2012-12-11 15:37:21 +0100

    Add missing reboot after restore default configuration

--- package/gargoyle/files/www/utility/do_restore_original.sh
+++ package/gargoyle/files/www/utility/do_restore_original.sh
@@ -8,5 +8,5 @@
 	eval $( gargoyle_session_validator -c "$POST_hash" -e "$COOKIE_exp" -a "$HTTP_USER_AGENT" -i "$REMOTE_ADDR" -r "login.sh" -t $(uci get gargoyle.global.session_timeout) -b "$COOKIE_browser_time"  )	
 
 	/sbin/firstboot >/dev/null 2>&1
-	
+	/sbin/reboot
 ?>
