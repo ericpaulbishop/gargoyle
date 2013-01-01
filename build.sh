009ceba08cbca81348b055a949840f3879032d11
Eric <ericpaulbishop@gmail.com>
2013-01-01 14:40:44 -0500

    bump openwrt attitude adjustment revision

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ set_version_variables()
 	# set svn revision number to use 
 	# you can set this to an alternate revision 
 	# or empty to checkout latest 
-	rnum=34379
+	rnum=34879
 
 	#set date here, so it's guaranteed the same for all images
 	#even though build can take several hours
