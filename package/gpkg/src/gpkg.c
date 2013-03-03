d7eb15b528187ee0e32fde4dcf244a5642f3c55f
Eric <ericpaulbishop@gmail.com>
2013-03-03 11:05:03 -0500

    more work on gpkg

--- package/gpkg/src/gpkg.c
+++ package/gpkg/src/gpkg.c
@@ -236,19 +236,27 @@ int recursively_install(char* pkg_name, char* install_root_name, char* link_to_r
 {
 	int err=0;
 	
-	string_map* pkg_data = get_string_map_element(package_data, pkg_name);
-	char* src_id       = get_string_map_element(pkg_data, "Source-ID");
-	char* pkg_filename = get_string_map_element(pkg_data, "Filename");
+	string_map* pkg_data    = get_string_map_element(package_data, pkg_name);
+	char* src_id            = get_string_map_element(pkg_data, "Source-ID");
+	char* pkg_filename      = get_string_map_element(pkg_data, "Filename");
+	char* install_root_path = get_string_map_element(conf->dest_names, install_root_name);
 	char* base_url = NULL;
 	char* pkg_dest = NULL;
 
-	if(src_id == NULL || pkg_filename == NULL)
+
+	char* info_dir            = NULL;
+	char* control_name_prefix = NULL;
+	char* list_file_name      = NULL;
+
+
+
+	if(src_id == NULL || pkg_filename == NULL || install_root_path)
 	{
 		err = 1;
 	}
 	if(err == 0)
 	{
-	
+		//determine source url
 		string_map* src_lists[2] = { conf->gzip_sources, conf->plain_sources };
 		int src_list_index;
 		for(src_list_index=0; src_list_index < 2 && base_url == NULL; src_list_index++)
@@ -260,6 +268,7 @@ int recursively_install(char* pkg_name, char* install_root_name, char* link_to_r
 
 	if(err == 0)
 	{
+		//download package
 		char* src_url  = dynamic_strcat(3, base_url, "/", pkg_filename);
 		pkg_dest = dynamic_strcat(3, tmp_dir, "/", pkg_name);
 		FILE* package_file = fopen(pkg_dest, "w");
@@ -272,28 +281,122 @@ int recursively_install(char* pkg_name, char* install_root_name, char* link_to_r
 		{
 			err = 1;
 		}
-		free(src_url);
-
-		
-
 		if(err == 1)
 		{
 			printf("ERROR: Could not download package %s\n", pkg_name);
 		}
+		free(src_url);
+
 
 	}
 	if(err == 0)
 	{
 		//check md5sum
 		char* md5sum = file_md5sum_alloc(pkg_dest);
+		char* expected_md5sum = (char*)get_string_map_element(pkg_data, "MD5Sum")
+		
 		printf("md5sum         = %s\n", md5sum);
 		printf("package md5sum = %s\n", (char*)get_string_map_element(pkg_data, "MD5Sum"));
+
+		if(md5sum == NULL || expected_md5sum == NULL)
+		{
+			printf("ERROR: Expected MD5Sum for %s not specified, cannot verify package\n", pkg_name);
+			err = 1;
+		}
+		else( strcmp(md5sum, expected_md5sum) != 0)
+		{
+			printf("ERROR: MD5Sum mismatch for %s package\n", pkg_name);
+			err = 1;
+		}
+		if(md5sum != NULL) { free(md5sum); }
+	}
+	if(err == 0)
+	{
+		// Extract list file contaiing list of files to install
+		info_dir            = dynamic_strcat(2, install_root_path, "/usr/lib/opkg/info");
+		control_name_prefix = dynamic_strcat(3, info_dir, "/", pkg_name);
+		list_file_name      = dynamic_strcat(4, info_dir, "/", pkg_name, ".list");
+		
+		mkdir_p(info_dir, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH );
+		FILE* list_file = fopen(list_file_name, "w");
+		deb_extract(	pkg_file,
+				list_file,
+				extract_quiet | extract_data_tar_gz | extract_list,
+				NULL,
+				NULL, 
+				&err);
+		fclose(list_file);
+		if(err)
+		{
+			rm_r(list_file_name);
+			fprintf(stderr, "ERROR: could not extract file list from packge %s.\n", pkg_name);
+			fprintf(stderr, "       package file may be corrupt\n\n");
+		}
+	}
+	if(err == 0)
+	{
+		//extract control files
+		deb_extract(	pkg_file,
+				stderr,
+				extract_control_tar_gz | extract_all_to_fs| extract_preserve_date | extract_unconditional,
+				control_name_prefix, 
+				NULL, 
+				&err);
+		if(err)
+		{
+			rm_r(list_file_name);
+			fprintf(stderr, "ERROR: could not extract control files from packge %s.\n", pkg_name);
+			fprintf(stderr, "       package file may be corrupt\n\n");
+		}
 
 	}
 	if(err == 0)
 	{
+		//check for file conflicts
+		list_file = fopen(list_file_name, "r");
+		unsigned long list_file_length;
+		char line_seps[] = {'\r', '\n'};
+		char* list_file_data =  read_entire_file(list_file, FILE_PATH_LEN, &list_file_length);
+		fclose(list_file);
+		
+		unsigned long num_list_lines;
+		char** list_file_lines = split_on_separators(list_file_data, line_seps , 2, -1, 0, &num_list_lines);
+		free(list_file_data);
+	
 
+		int install_root_len = strlen(install_root);
+		char* fs_terminated_install_root = install_root[install_root_len-1] == '/' ? strdup(install_root) : dynamic_strcat(2, install_root, "/");
+		list_file = fopen(list_file_name, "w");
+		int line_index;
+		for(line_index=0; line_index < num_list_lines && (!err) ; line_index++)
+		{
+			int line_len = strlen( list_file_lines[line_index] );
+			if(line_len > 2)
+			{
+				if(list_file_lines[line_index][0] == '.' && list_file_lines[line_index][1] == '/' && list_file_lines[line_index][line_len-1] != '/')
+				{
+					char* adjusted_file_path = dynamic_strcat(2, install_root, fs_terminated_install_root, list_file_lines[line_index] + 2);
+					fprintf(list_file, "%s\n", adjusted_file_path);
+					err = path_exists(adjusted_file_path) ? 1 : 0;
+					if(err)
+					{
+						fprintf(stderr, "ERROR: file '%s'\n", adjusted_file_path);
+						fprintf(stderr, "       from package %s already exists.\n\n", pkg_name);
+					}
+					free(adjusted_file_path);
+	
+				}
+			}
+		}
+		fclose(list_file);
+		if(err)
+		{
+			rm_r(list_file_name);
+		}
 	}
+
+
+		
 
 
 
