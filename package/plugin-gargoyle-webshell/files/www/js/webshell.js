1791add9903fd85642055e08068ca5adb2f835e7
Cezary Jackiewicz <cezary.jackiewicz@gmail.com>
2013-03-04 15:41:09 +0100

    Add plugin webshell

---
+++ package/plugin-gargoyle-webshell/files/www/js/webshell.js
@@ -0,0 +1,28 @@
+/*
+ * This program is copyright © 2012 Cezary Jackiewicz and is distributed under the terms of the GNU GPL 
+ * version 2.0 with a special clarification/exception that permits adapting the program to 
+ * configure proprietary "back end" software provided that all modifications to the web interface
+ * itself remain covered by the GPL. 
+ * See http://gargoyle-router.com/faq.html#qfoss for more information
+ */
+
+function runCmd()
+{
+	var Commands = [];
+
+	Commands.push(document.getElementById("cmd").value);
+
+	setControlsEnabled(false, true);
+
+	var param = getParameterDefinition("commands", Commands.join("\n")) + "&" + getParameterDefinition("hash", document.cookie.replace(/^.*hash=/,"").replace(/[\t ;]+.*$/, ""));
+	var stateChangeFunction = function(req)
+	{
+		if(req.readyState == 4)
+		{
+			document.getElementById("output").value = req.responseText;
+			setControlsEnabled(true);
+		}
+	}
+
+	runAjax("POST", "utility/run_commands.sh", param, stateChangeFunction);
+}
