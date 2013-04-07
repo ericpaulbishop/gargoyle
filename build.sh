f249d0d5ed44daeb07951a8de9639e44a3dfeb3d
Eric <ericpaulbishop@gmail.com>
2013-04-06 20:29:09 -0400

    bump to latest openwrt AA version and add support for tp-link mr3220-v2 and mr3420-v2 (tested and working on mr3220-v2)

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ set_version_variables()
 	# set svn revision number to use 
 	# you can set this to an alternate revision 
 	# or empty to checkout latest 
-	rnum=35631
+	rnum=36089
 
 	#set date here, so it's guaranteed the same for all images
 	#even though build can take several hours
