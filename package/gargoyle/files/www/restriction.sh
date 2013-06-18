773e8ca42af312d16a01201d142e24cba73185c9
BashfulBladder <bashfulbladder@gmail.com>
2013-06-18 16:42:51 -0400

    restrictions.sh: transition to new i18n form; another Sun,Mon,Tue field requiring more intrusive change

--- package/gargoyle/files/www/restriction.sh
+++ package/gargoyle/files/www/restriction.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/haserl
-<?
-	# This program is copyright © 2008 Eric Bishop and is distributed under the terms of the GNU GPL
+#!/usr/bin/haserl2
+<%
+	# This program is copyright © 2008-2013 Eric Bishop and is distributed under the terms of the GNU GPL
 	# version 2.0 with a special clarification/exception that permits adapting the program to
 	# configure proprietary "back end" software provided that all modifications to the web interface
 	# itself remain covered by the GPL.
 	# See http://gargoyle-router.com/faq.html#qfoss for more information
 	eval $( gargoyle_session_validator -c "$COOKIE_hash" -e "$COOKIE_exp" -a "$HTTP_USER_AGENT" -i "$REMOTE_ADDR" -r "login.sh" -t $(uci get gargoyle.global.session_timeout) -b "$COOKIE_browser_time"  )
-	gargoyle_header_footer -h -s "firewall" -p "restriction" -c "internal.css" -j "table.js restrictions.js" gargoyle firewall
+	gargoyle_header_footer -h -s "firewall" -p "restriction" -c "internal.css" -j "table.js restrictions.js" -z "restrictions.js" gargoyle firewall
 
-?>
+%>
 
 <script>
 <!--
@@ -18,60 +18,52 @@
 
 <form>
 	<fieldset>
-		<legend class="sectionheader">Access Restrictions</legend>
+		<legend class="sectionheader"><%~ restrictions.ARSect %></legend>
 
-		<span id="add_rule_label" style="text-decoration:underline" >New Restriction Rule:</span>
+		<span id="add_rule_label" style="text-decoration:underline" ><%~ NRRule %>:</span>
 
 		<div>
-			<?
-			sed -e '/<L7OPTIONS>/,$ d' templates/restriction_template
-			sed -e '/^#/ d'  -e "s/\([^ ]* \)\(.*\)/<option value='\1'>\2<\/option>/" /etc/l7-protocols/l7index
-			sed -e '1,/<L7OPTIONS>/ d' templates/restriction_template
-			?>
+			<%in templates/restriction_template %>
 
 			<div>
-				<input type="button" id="add_restriction_button" class="default_button" value="Add New Rule" onclick='addNewRule("restriction_rule", "rule_")' />
+				<input type="button" id="add_restriction_button" class="default_button" value="<%~ ANRule %>" onclick='addNewRule("restriction_rule", "rule_")' />
 			</div>
 		</div>
 
 		<div id='internal_divider1' class='internal_divider'></div>
 
-		<span id="current_rule_label" style="text-decoration:underline" >Current Restrictions:</span>
+		<span id="current_rule_label" style="text-decoration:underline" ><%~ CRestr %>:</span>
 
 		<div id="rule_table_container"></div>
 
 	</fieldset>
 
 		<fieldset>
-		<legend class="sectionheader">Exceptions (White List)</legend>
+		<legend class="sectionheader"><%~ EWSect %></legend>
 
-		<span id="add_exception_label" style="text-decoration:underline" >New Exception:</span>
+		<span id="add_exception_label" style="text-decoration:underline" ><%~ NExcp %>:</span>
 
 		<div>
-			<?
-			sed -e '/<L7OPTIONS>/,$ d' templates/whitelist_template
-			sed -e '/^#/ d'  -e "s/\([^ ]* \)\(.*\)/<option value='\1\2'>\2<\/option>/" /etc/l7-protocols/l7index
-			sed -e '1,/<L7OPTIONS>/ d' templates/whitelist_template
-			?>
+			<%in templates/whitelist_template %>
 
 			<div>
-				<input type="button" id="add_restriction_button" class="default_button" value="Add New Rule" onclick='addNewRule("whitelist_rule", "exception_")' />
+				<input type="button" id="add_restriction_button" class="default_button" value="<%~ ANRule %>" onclick='addNewRule("whitelist_rule", "exception_")' />
 			</div>
 		</div>
 
 		<div id='internal_divider1' class='internal_divider'></div>
 
-		<span id="current_exceptions_label" style="text-decoration:underline" >Current Exceptions:</span>
+		<span id="current_exceptions_label" style="text-decoration:underline" ><%~ CExcp %>:</span>
 
 		<div id="exception_table_container"></div>
 
 	</fieldset>
 	<div id="bottom_button_container">
-		<input type='button' value='Save Changes' id="save_button" class="bottom_button" onclick='saveChanges()' />
-		<input type='button' value='Reset' id="reset_button" class="bottom_button" onclick='resetData()'/>
+		<input type='button' value='<%~ SaveChanges %>' id="save_button" class="bottom_button" onclick='saveChanges()' />
+		<input type='button' value='<%~ Reset %>' id="reset_button" class="bottom_button" onclick='resetData()'/>
 	</div>
 
-	<span id="update_container" >Please wait while new settings are applied. . .</span>
+	<span id="update_container" ><%~ WaitSettings %></span>
 </form>
 
 <!-- <br /><textarea style="margin-left:20px;" rows=30 cols=60 id='output'></textarea> -->
@@ -82,6 +74,6 @@
 //-->
 </script>
 
-<?
+<%
 	gargoyle_header_footer -f -s "firewall" -p "restriction"
-?>
+%>
---
+++ package/plugin-gargoyle-i18n-Spanish-ES/files/www/i18n/Spanish-ES/restrictions.js
@@ -0,0 +1,67 @@
+﻿/*
+ * UTF-8 (with BOM) Spanish-ES text strings for restrictions.sh html elements
+ */
+
+restStr.ARSect="Restricciones de Acceso";
+restStr.NRRule="Nueva Regla de Restricción";
+restStr.ANRule="Añadir Nueva Regla";
+restStr.CRestr="Restricciones Actuales";
+restStr.EWSect="Excepciones (Lista Blanca)";
+restStr.NExcp="Nueva Excepción";
+restStr.CExcp="Excepciones Actuales";
+
+restStr.ERSect="Editar Regla de Restricción";
+restStr.EESect="Editar Regla de Excepción";
+
+//templates
+restStr.RDesc="Descripción de La Regla";
+restStr.RAppl="Regla Se Aplica A";
+restStr.AHsts="Todos Los Anfitriones";
+restStr.EHsts="Todos Los Anfitriones de Excepción";
+restStr.OHsts="Sólo al Siguiente Anfitriones";
+restStr.HstAddr="Especifique un IP, rango de IP o dirección MAC";
+restStr.Schd="Horario";
+restStr.ADay="Todo el Día";
+restStr.EDay="Todos los Días";
+restStr.DSchd="Horario Repite Todos los Días";
+restStr.WSchd="Horario Repite Semanal";
+restStr.DActv="Días Activos";
+restStr.HActv="Horas Activo";
+restStr.DHActv="Días y Horas Activas";
+restStr.Sample="e.g. lun 00:30 - jue 13:15, vie 14:00 - vie 15:00";
+restStr.RRsrc="Recursos Restringidos";
+restStr.NetAcc="Todo Acceso a la Red";
+restStr.RemIP="Remoto IP(s)";
+restStr.RemPrt="Remoto Port(s)";
+restStr.LclPrt="Local Port(s)";
+restStr.TrProto="Protocolo de Transporte";
+restStr.ApProto="Protocolo de Aplicación";
+restStr.WebURL="Sitio Web URL(s)";
+restStr.BlAll="Bloque Todo";
+restStr.BlOny="Bloque Sólo";
+restStr.BlExc="Bloque Todo Excepto";
+restStr.BlTCP="Bloque TCP";
+restStr.BlUDP="Bloque UDP";
+restStr.FUExt="Completo URL coincide exactamente";
+restStr.FUCnt="Completo URL contiene";
+restStr.FURgx="Completo URL coincide Regex";
+restStr.DmExt="Dominio coincide exactamente";
+restStr.DmCon="Dominio contiene";
+restStr.DmRgx="Dominio coincide Regex";
+restStr.ESect="Excepcion Descripción";
+restStr.EAppl="Excepcion Se Aplica A";
+restStr.PRsrc="Recursos Autorizados";
+restStr.PmAll="Permite Todos";
+restStr.PmOny="Permite Sólo";
+restStr.PmExc="Permite Todo Excepto";
+restStr.PmTCP="Permite TCP";
+restStr.PmUDP="Permite UDP";
+
+//javascript
+restStr.ARErr="No se pudo añadir la regla.";
+restStr.IAErr="ERROR: No Válido Dirección";
+restStr.UPrt="URL Parte";
+restStr.MTyp="Tipo de Coincide";
+restStr.MTExp="Coincide de Texto / Expresión";
+restStr.UMZErr="ERROR: longitud de partido URL debe ser mayor que cero";
+restStr.UChErr="ERROR: partido URL no puede contener cotización o caracteres de nueva línea";
