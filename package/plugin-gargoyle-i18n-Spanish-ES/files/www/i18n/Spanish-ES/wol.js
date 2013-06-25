f63de309ce22280ec75b86b25053e1226819be3a
BashfulBladder <bashfulbladder@gmail.com>
2013-06-25 03:58:50 -0400

    wol.sh: transition to i18n form

---
+++ package/plugin-gargoyle-i18n-Spanish-ES/files/www/i18n/Spanish-ES/wol.js
@@ -0,0 +1,8 @@
+﻿/*
+ * UTF-8 (with BOM) Spanish-ES text strings for wol.sh html elements
+ */
+
+wolS.WLSect="Wake on LAN (Despierta)";
+wolS.WLHelp="<em>Wake on LAN (WoL)</em> es un estándar de redes de computadoras Ethernet que permite a una computadora puede activar o despertado por un mensaje de red. El mensaje es enviado por un programa ejecutado en el enrutador en la misma red de área local.</p><p>Este mensaje especial a la red se llama el paquete mágico y es contiene la dirección MAC del equipo de destino. El equipo espera a escuchar un paquete mágico que le había dirigido y luego inicia el sistema raíz hacia arriba.</p><p>Wake on LAN por lo general tiene que ser activado en la sección de administración de energía de utilidad de configuración del BIOS de un PC de la placa madre. Además, con el fin de obtener Wake on LAN para trabajar a veces se necesita para habilitar esta característica en la tarjeta de interfaz de red o el controlador de dispositivo de silicio de a bordo.";
+wolS.WkUp="Despiértese";
+wolS.WAlr="Despiértese solicitud enviada.";
