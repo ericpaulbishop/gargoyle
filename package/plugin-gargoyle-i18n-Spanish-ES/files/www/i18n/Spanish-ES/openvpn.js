03a31daaeaeee8b5f31794ac5acbd00480926a32
BashfulBladder <bashfulbladder@gmail.com>
2013-07-06 23:42:49 -0400

    …and now the translation pages for plugin-gargoyle-openvpn

---
+++ package/plugin-gargoyle-i18n-Spanish-ES/files/www/i18n/Spanish-ES/openvpn.js
@@ -0,0 +1,109 @@
+﻿/*
+ * UTF-8 (with BOM) Spanish-ES text strings for OpenVPN elements
+ */
+
+ovpnS.OCfg="Configuración de OpenVPN";
+ovpnS.ODis="OpenVPN Desactivado";
+ovpnS.OClt="Cliente de OpenVPN";
+ovpnS.OSrv="Servidor de OpenVPN";
+ovpnS.OSts="Estado de OpenVPN";
+ovpnS.OSCfg="Servidor de OpenVPN: Configuración";
+ovpnS.OInIP="OpenVPN IP Interno";
+ovpnS.OIMsk="OpenVPN Máscara de Subred Interna";
+ovpnS.OPrt="OpenVPN Puerto";
+ovpnS.OProto="OpenVPN Protocolo";
+ovpnS.OCiph="OpenVPN Cifra";
+ovpnS.CCTr="Tráfico Cliente-a-Cliente";
+ovpnS.CtoC="Permiten a los Clientes Comunicarse Entre Sí";
+ovpnS.CtoS="Clientes Pueden Comunicarse con Servidor Sólo";
+ovpnS.LSAc="Acceso Subred LAN";
+ovpnS.CtoH="Permiten Clientes Acceder a los Hosts LAN";
+ovpnS.CnoL="Clientes No Pueden Tener Acceso LAN";
+ovpnS.CredR="Credencial de Re-Uso";
+ovpnS.CredSC="Credenciales Son Específicos para Cada Cliente";
+ovpnS.CredMC="Credenciales Pueden Ser Utilizados Por Varios Clientes";
+ovpnS.CUse="Clientes utilizan VPN para";
+ovpnS.ATrff="Todo Tráfico Cliente";
+ovpnS.HTrff="Sólo Tráfico Destinado para Máquinas detrás de VPN";
+ovpnS.OSAC="Servidor de OpenVPN: Clientes Permitido";
+ovpnS.CClnt="Clientes Configurados Actualmente";
+ovpnS.ZipCred="Después de generar la configuración del cliente, haga clic en Descargar para obtener el archivo zip que contiene las credenciales necesarias, y colocar en carpeta de configuración de OpenVPN de su cliente";
+ovpnS.CfgCred="Configurar un Nuevo Cliente / Conjunto de Credenciales";
+ovpnS.ClntN="Nombre del Cliente";
+ovpnS.ClntIP="Cliente IP interna";
+ovpnS.ClntConn="Cliente Se Conecta a";
+ovpnS.ClntSubN="Detrás de Subred del Cliente";
+ovpnS.NoSub="No Definido Subred";
+ovpnS.RtSub="Encaminar Subred Abajo";
+ovpnS.SubIP="Subred IP";
+ovpnS.SubM="Máscara de subred";
+ovpnS.UpCfgF="Cargar Archivo(s) de Configuración de Cliente";
+ovpnS.CfgMan="Configurar el Cliente de Forma Manual";
+ovpnS.UpFmt="Subir Formato";
+ovpnS.SZipF="Solo Archivo Zip";
+ovpnS.CfgF="Archivos de Configuración Individuales";
+ovpnS.ZipF="Archivo Zip";
+ovpnS.OCfgF="Archivo de Configuración OpenVPN";
+ovpnS.CACF="Archivo de CA Certificado";
+ovpnS.CCertF="Archivo de Certificado de Cliente";
+ovpnS.CKeyF="Client Archivo de Clave";
+ovpnS.TAKeyF="Archivo de Clave de TLS-Auth";
+ovpnS.UseTAK="Uso Clave TLS-Auth";
+ovpnS.OSrvAddr="Dirección del Servidor OpenVPN";
+ovpnS.OSrvPrt="Puerto del Servidor OpenVPN";
+ovpnS.Othr="Otro";
+ovpnS.Cphr="Cifra";
+ovpnS.Keyopt="Tamaño de Clave (opcional)";
+ovpnS.CfgUpd="Configuración a continuación se actualiza automáticamente a partir de parámetros especificados anteriormente";
+ovpnS.CACert="Certificado de CA";
+ovpnS.CCert="Certificado de Cliente";
+ovpnS.CKey="Clave de Cliente";
+ovpnS.TAKey="Clave de TLS-Auth";
+ovpnS.TADir="TLS-Auth Dirección";
+ovpnS.Clnt="Cliente";
+ovpnS.Symm="Omitido (Simétrico)";
+
+//javascript
+ovpnS.CryptoWaitMsg="Esta es la primera vez que se ha configurado un servidor OpenVPN.\n\nSe llevará a 5-10 minutos para generar los parámetros criptográficos necesarios. Este es un tiempo de espera de una sola vez - actualizaciones después de que éste será rápido.\n\n¿Desea continuar?";
+ovpnS.SubMis="Discrepancia de Subred del Cliente";
+ovpnS.ExpSubN="El OpenVPN espera que el router tenga una subred de";
+ovpnS.ActSubN="pero el router está configurado con una subred de";
+ovpnS.WantQ="¿Quieres...";
+ovpnS.Switch="Cambiar Router a subred se esperaba, con IP";
+ovpnS.KeepC="Mantenga Subred Actual y Continuar";
+ovpnS.SrvPrtErr="Conflictos de puerto OpenVPN servidor con";
+ovpnS.SrvAddErr="Dirección del servidor no está definido";
+ovpnS.OPrtErr="OpenVPN puerto debe estar entre 1-65535";
+ovpnS.GTAPErr="Gárgola no admite configuraciones TAP OpenVPN";
+ovpnS.RunC="Ejecutando, Conectado";
+ovpnS.RunNC="Ejecutando, No Conectado";
+ovpnS.RunNot="No Se Está Ejecutando";
+ovpnS.IntIP="IP Interna\n(Subred Enrutada)";
+ovpnS.CfgCredF="Credenciales\ny Archivos de Configuración";
+ovpnS.Dload="Descargar";
+ovpnS.DDNS="DNS Dinámico";
+ovpnS.WANIP="WAN IP";
+ovpnS.OthIPD="Otro IP o Dominio (especificado abajo)";
+ovpnS.ClntIntIP="IP interna de Especificado Cliente";
+ovpnS.OSubErr="no está en OpenVPN subred";
+ovpnS.AddCErr="No se pudo añadir la configuración del cliente.";
+ovpnS.UpCErr="No se pudo actualizar la configuración del cliente.";
+
+//openvpn_allowed_client_edit.sh
+ovpnS.EditOCS="Editar Configuración del Cliente OpenVPN";
+
+//openvpn_upload_client.sh (handled by shell scripts)
+ovpnS.uc_CA_f="No se pudo encontrar el archivo de CA";
+ovpnS.uc_crt_f="No se pudo encontrar el archivo de certificado";
+ovpnS.uc_key_f="No se pudo encontrar el archivo de claves";
+ovpnS.uc_cfg_f="No se pudo encontrar el archivo de configuración";
+ovpnS.uc_TAP_Err="Gárgola no admite configuraciones TAP OpenVPN";
+ovpnS.uc_conn_Err="Parámetros guardados OpenVPN pero no pudo conectar. Vuelva a comprobar la configuración.";
+
+//openvpn_connections.sh
+ovpnS.ConnOC="Clientes OpenVPN Conectados";
+
+//openvpn_connections.js
+ovpnS.ConnFr="Conectado Desde";
+ovpnS.ConnSc="Conectado Desde Que";
+ovpnS.NoCConn="No Hay Clientes Conectados";
