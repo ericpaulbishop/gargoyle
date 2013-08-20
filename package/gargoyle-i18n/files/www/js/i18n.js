90d7a857b3d6bb22010f37b61acb6b3a4c038e71
Eric <ericpaulbishop@gmail.com>
2013-08-20 15:28:27 -0400

    some work towards revamping i18n firstboot screen

--- package/gargoyle-i18n/files/www/js/i18n.js
+++ package/gargoyle-i18n/files/www/js/i18n.js
@@ -1,5 +1,5 @@
 /*
- * This program is copyright © 2013 BashfulBladder and is distributed under the terms of the GNU GPL 
+ * This program is copyright © 2013 BashfulBladder and Eric Bishop and is distributed under the terms of the GNU GPL 
  * version 2.0 with a special clarification/exception that permits adapting the program to 
  * configure proprietary "back end" software provided that all modifications to the web interface
  * itself remain covered by the GPL. 
@@ -10,24 +10,26 @@ var intS=new Object();
 //
 //  GenLangContainer initiates generating the language container with 2 flanking dividing lines
 //
-function GenLangContainer() {
-	function genDivider() { var a_div=document.createElement('div'); a_div.className="internal_divider"; return a_div }
+function GenLangContainer()
+{
 	var lc=document.getElementById("lang_container");
+	lc.appendChild(GenLangDiv(HaveNet));
 	lc.appendChild(genDivider());
-    lc.appendChild(GenLangDiv(HaveNet));
-    lc.appendChild(genDivider());
 }
 
 //
 //  GenLangMenu uses the javascript output of gpkg to provide info on available packages & builds a table from the multi-dimensional
 //  associative array.
 //
-function GenLangMenu() {
+function GenLangMenu()
+{
 	var columnNames = [intS.Lang, intS.Desc, ''];
 	var TableData = new Array();
 
-	for(pkgName in pkg_info) {
-		for (ver in pkg_info[pkgName]) {
+	for(pkgName in pkg_info)
+	{
+		for (ver in pkg_info[pkgName])
+		{
 			//since firstboot.sh only comes up on a pristine router, the first version *should* be the most current - except in a case of failsafe...
 			var pStatus=pkg_info[pkgName][ver]["Status"].split(" ");
 			TableData.push([pkgName,
@@ -49,7 +51,8 @@ function GenLangMenu() {
 //    'Save' is clicked, the form has its .submit function called which... ? targets the Iframe to download the file in POST?
 //       that part is opaque - the html elements of the upgrade.sh page were followed & javascript-ized
 //
-function GenLangForm() {
+function GenLangForm()
+{
 	var a_div=document.createElement('div');
 	var a_form=document.createElement("form");
 	var a_iframe=document.createElement('iframe');
@@ -68,8 +71,8 @@ function GenLangForm() {
 	a_iframe.style.display="none";
 	a_iframe.src="#";
 	a_iframe.name="get_lfile";
-    a_iframe.id="get_lfile";
-    
+ 	a_iframe.id="get_lfile";
+
 	a_input.type='file';
 	a_input.name='lfile';
 	a_input.id='lfile';
@@ -100,52 +103,35 @@ function GenLangForm() {
 	return a_div
 }
 
-function GenPicto(path, hgt, wdt, top) {
-	var a_embed = document.createElement('embed');
-	a_embed.setAttribute('height', hgt);
-	a_embed.setAttribute('width', wdt);
-	a_embed.setAttribute('src', path);
-	a_embed.style.marginTop=top+"px";
-	a_embed.setAttribute('pluginspage', "http://www.adobe.com/svg/viewer/install/");
-	a_embed.setAttribute('type', "image/svg+xml");
-	a_embed.setAttribute('encoding', "gzip");
-	return a_embed
-}
 
-function GenLangDiv(field) {
+function GenLangDiv(field)
+{
 	var lang_back = ["語","언어","ภาษา","भाषा","لغة","שפה","زبان","язык","γλώσσα",
-					"dil","Sprache","kieli","język","langue","lingua","lengua","language"];
+					"dil","sprache","kieli","język","langue","lingua","lengua","language"];
 
 	var a_div=document.createElement('div');
-	var b_div=document.createElement('div');
-	a_div.style.textAlign="center";
-	b_div.style.width='auto';
-	b_div.style.height='100px';
-	b_div.style.fontSize="30px";
-	b_div.style.lineHeight="27px";
-	for (var i=0; i < lang_back.length; i++) {
-		b_div.innerHTML+=lang_back[i]+ " ";
-	}
-	a_div.appendChild(b_div);
-	
+		
 	//connected or disconnected connection
-	if (field == 1) {
-		a_div.appendChild(GenPicto("i18n/graphics/connected.svg", 100, 400, 0));
+	if (field == 1)
+	{
 		a_div.appendChild(GenLangMenu());
 	}
-	if (field == 0 || field == 1) {
-		a_div.appendChild(GenPicto("i18n/graphics/disconnected.svg", 100, 400, 40));
+	if (field == 0 || field == 1)
+	{
 		a_div.appendChild(GenLangForm());
 	}
 	
 	return a_div;
 }
 
-function do_get_lfile() {
+function do_get_lfile()
+{
 	var fname=document.getElementById('lfile').value;
 	
-	if (fname.length > 0 ) {
-		if (fname.match(/plugin-gargoyle-i18n/) && fname.substring(fname.lastIndexOf('.') + 1) == 'ipk') {
+	if (fname.length > 0 )
+	{
+		if (fname.match(/plugin-gargoyle-i18n/) && fname.substring(fname.lastIndexOf('.') + 1) == 'ipk')
+		{
 			setControlsEnabled(false, true, intS.UpMsg);
 
 			document.getElementById('lfile_fname').value = fname;
@@ -155,14 +141,16 @@ function do_get_lfile() {
 	}
 }
 
-function InstallLang() {
+function InstallLang()
+{
 	console.log(this.parentNode.parentNode.firstChild.innerHTML);
 	var pkg = this.parentNode.parentNode.firstChild.innerHTML;
 	var cmd = [ "sh /usr/lib/gargoyle/remove_gargoyle_package.sh " + pkg ];
 	//execute(cmd);
 }
 
-function ldone() {
+function ldone()
+{
 	setControlsEnabled(true);
 	window.location.reload(true);
 }
@@ -170,11 +158,14 @@ function ldone() {
 function createInstallButton(type)
 {
 	var inButton = createInput("button");
-	if (type == 1) {
+	if (type == 1)
+	{
 		inButton.value = UI.Install;
 		inButton.className="default_button";
 		inButton.onclick = InstallLang;
-	} else {
+	}
+	else
+	{
 		inButton.value = intS.Instd;
 		inButton.className="default_button_disabled";
 		inButton.onclick = ""
@@ -185,11 +176,14 @@ function createInstallButton(type)
 function createUseButton(type)
 {
 	var useButton = createInput("button");
-	if (type == 1) {
+	if (type == 1)
+	{
 		useButton.value = intS.Acv8;
 		useButton.className="default_button";
 		useButton.onclick = activateLang;
-	} else {
+	}
+	else
+	{
 		useButton.value = intS.Actv;
 		useButton.className="default_button_disabled";
 		useButton.onclick = ""
@@ -199,6 +193,7 @@ function createUseButton(type)
 
 function resetData()
 {
+	
 	var tableContainer = document.getElementById('lang_table_container');
 	tableContainer.appendChild(GenLangDiv(2));
 	
@@ -218,6 +213,7 @@ function resetData()
 	//	tableContainer.removeChild(tableContainer.firstChild);
 	//}
 	tableContainer.appendChild(Table);
+	
 }
 
 function activateLang(row, action)
