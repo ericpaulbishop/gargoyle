5a2d5d538318b26409d63a3fbcc78fe6c83dacf3
WildRat <santiago.bikers@gmail.com>
2013-09-11 14:35:16 +0300

    add russian translation. first version.

---
+++ package/plugin-gargoyle-i18n-Russian-RU/files/www/i18n/Russian-RU/login.js
@@ -0,0 +1,21 @@
+﻿/*
+ * UTF-8 (with BOM) Russian-RU text strings for login.sh html elements
+ */
+
+logS.LSect="Подключение";
+logS.EAdmP="Введите пароль администратора";
+logS.YQot="Ваша квота";
+logS.NQot="Квота всей сети";
+logS.CTime="Дата и время";
+
+//javascript
+logS.passErr="ОШИБКА: Вы должны ввести пароль";
+logS.Lging="Подключение";
+logS.SExp="Время сеанса истекло";
+logS.InvP="Неверный пароль";
+logS.LOut="Сеанс завершен";
+logS.Qnam=["всего", "загруженно", "отправленно" ];
+logS.of="из";
+logS.fQuo="по квоте";
+logS.husd="было использовано";
+logS.qusd="квота исчерпана";
