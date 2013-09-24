984556228c57e998fe621adc68e182623b25934e
Eric <ericpaulbishop@gmail.com>
2013-09-24 00:52:37 -0400

    set more packages, not just kernel modules, to build as modules in all target profiles

--- dev-utils/enable_kernel_module_packages.pl
+++ dev-utils/enable_kernel_module_packages.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 # This script takes targets dir as argument, then enables 
-# all kernel packages that are configured as a module or built-in in ANY
+# all packages that are configured as a module or built-in in ANY
 # subtarget as a module in ALL subtargets where they are not currently selected at all
 
 
@@ -14,25 +14,25 @@ foreach my $arch (@arches)
 	my @profiles = glob("$arch/profiles/*");
 	if(scalar(@profiles) > 1)
 	{
-		my $kmodPackages = {};
-		$profileKmodPackages = {};
+		my $packages = {};
+		$profilePackages = {};
 		foreach $profile (@profiles)
 		{
 			my $config = "$profile/config";
 			if ( -e "$config")
 			{
-				$profileKmodPackages->{$profile} = {};
+				$profilePackages->{$profile} = {};
 				open IN, "<$config";
 				while (my $line = <IN>)
 				{
 					chomp $line;
-					if($line =~ /CONFIG_PACKAGE_kmod\-/ && $line !~ /is not set/ )
+					if($line =~ /CONFIG_/ && $line !~ /is not set/ && $line !~ /CONFIG_TARGET/ )
 					{
 						my $package = $line;
-						$package =~ s/^.*CONFIG_PACKAGE_//g;
+						$package =~ s/^.*CONFIG_//g;
 						$package =~ s/[=\t ]+.*$//g;
-						$kmodPackages->{$package} = 1;
-						$profileKmodPackages->{$profile}->{$package} = 1;
+						$packages->{$package} = 1;
+						$profilePackages->{$profile}->{$package} = 1;
 						print "$package\n";
 					}
 				}
@@ -44,7 +44,7 @@ foreach my $arch (@arches)
 			my $config = "$profile/config";
 			if ( -e "$config")
 			{
-				my $existingKmods =  $profileKmodPackages->{$profile};
+				my $existingPackages =  $profilePackages->{$profile};
 				open IN, "<$config";
 				open OUT, ">$config.tmp";
 				while (my $line = <IN>)
@@ -52,29 +52,29 @@ foreach my $arch (@arches)
 					chomp $line;
 					
 					my $newLine = $line;
-					if($line =~ /CONFIG_PACKAGE_kmod\-/ && $line =~ /is not set/ )
+					if($line =~ /CONFIG_/ && $line =~ /is not set/ )
 					{
 						my $package = $line;
-						$package =~ s/^.*CONFIG_PACKAGE_//g;
+						$package =~ s/^.*CONFIG_//g;
 						$package =~ s/[=\t ]+.*$//g;
 						print "not set package = $package\n";
 
-						if(defined($kmodPackages->{$package}))
+						if(defined($packages->{$package}))
 						{
-							$newLine="CONFIG_PACKAGE_${package}=m";
+							$newLine="CONFIG_${package}=m";
 							print "$newLine\n";
-							$existingKmods->{$package}=1;
+							$existingPackages->{$package}=1;
 						}
 					}
 					print OUT "$newLine\n";
 				}
 				close IN;
 
-				foreach my $kmod (keys %$kmodPackages)
+				foreach my $p (keys %$packages)
 				{
-					if(not defined($existingKmods->{$kmod}))
+					if(not defined($existingPackages->{$p}))
 					{
-						print OUT "CONFIG_PACKAGE_${kmod}=m\n";
+						print OUT "CONFIG_${p}=m\n";
 					}
 				}
 				close OUT;
