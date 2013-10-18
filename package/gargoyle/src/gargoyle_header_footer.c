7de87361286dcc79e5d33f74e8953bf85e8dfd44
Eric <ericpaulbishop@gmail.com>
2013-10-18 10:53:24 -0400

    fix javascript i18n link in headers when path is a symlink

--- package/gargoyle/src/gargoyle_header_footer.c
+++ package/gargoyle/src/gargoyle_header_footer.c
@@ -353,10 +353,11 @@ int main(int argc, char **argv)
 					printf("\t<script language=\"javascript\" type=\"text/javascript\" src=\"i18n/%s/%s?%s\"></script>\n", fallback_lang, all_lstr_js[lstr_js_index], gargoyle_version);
 				}
 				char* tran_file = dynamic_strcat(5, web_root, "/i18n/", active_lang, "/", all_lstr_js[lstr_js_index]);
-				if (path_exists(tran_file) == PATH_IS_REGULAR_FILE) {
+				if (path_exists(tran_file) == PATH_IS_REGULAR_FILE || path_exists(tran_file) == PATH_IS_SYMLINK)
+				{
 					printf("\t<script language=\"javascript\" type=\"text/javascript\" src=\"/i18n/%s/%s?%s\"></script>\n", active_lang, all_lstr_js[lstr_js_index], gargoyle_version);
 				}
-                free(tran_file);
+				free(tran_file);
 			}
 		}
 #endif
