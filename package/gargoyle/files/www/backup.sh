0a8d95c804316023f13d775978c4dce8bb3a7394
Cezary Jackiewicz <cezary.jackiewicz@gmail.com>
2013-12-29 12:58:15 +0100

    haserl: remove upload-target, this is not a valid option

--- package/gargoyle/files/www/backup.sh
+++ package/gargoyle/files/www/backup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/haserl --upload-limit=4096 --upload-target=/tmp/ --upload-dir=/tmp/
+#!/usr/bin/haserl --upload-limit=4096 --upload-dir=/tmp/
 <%
 	# This program is copyright © 2008-2013 Eric Bishop and is distributed under the terms of the GNU GPL
 	# version 2.0 with a special clarification/exception that permits adapting the program to
