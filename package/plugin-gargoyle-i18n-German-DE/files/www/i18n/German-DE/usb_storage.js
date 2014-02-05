7b937ac1f63e0eda57fbc0773f687ae71bcc702b
Thomas Knepper <thknepper@gmx.de>
2014-02-05 22:26:09 +0100

    i18n/German: more files translated / updates

---
+++ package/plugin-gargoyle-i18n-German-DE/files/www/i18n/German-DE/usb_storage.js
@@ -0,0 +1,98 @@
+﻿/*
+ * UTF-8 (with BOM) German-DE text strings for USB Storage html elements
+ */
+
+usbSStr.SDisk="Shared Disks";
+usbSStr.Nomdsk="No mounted USB disks detected";
+usbSStr.WFTP="Allow Access to FTP From WAN";
+usbSStr.WpFTP="Allow Passive FTP From WAN on ports";
+usbSStr.CFWkg="CIFS Workgroup";
+usbSStr.CFUsr="CIFS / FTP Users";
+usbSStr.NewU="Neuer Nutzer";
+usbSStr.AddU="Nutzer hinzufügen";
+usbSStr.Pasw="Passwort";
+usbSStr.CfPass="Bestätige Passwort";
+usbSStr.ADir="Add Shared Disk / Directory";
+usbSStr.ADsk="Add Shared Disk";
+usbSStr.CShare="Currently Shared Disks";
+usbSStr.Umnt="Unmount";
+usbSStr.UmntB="Unmount All USB Disks";
+usbSStr.UmntWarn="USB Disks should be unmounted before removal from the router. USB Disks still connected will be automatically remounted after next router reboot.";
+usbSStr.FDisk="Format Disk";
+usbSStr.NoUmntDev="<p>No attached, unmounted drives detected.</p><p>You must unmount drives before attempting to format them.</p>";
+usbSStr.FmtWarn="WARNING: Formatting a disk will permanently wipe out all contents of that disk.<p>Disk will be formatted for storage with EXT4 filesystem<br/>EXT4 may not be readable if USB drive is removed and attached to a Windows/Mac computer</p>";
+usbSStr.DskForm="Disk to format";
+usbSStr.PSwap="Percent Swap";
+usbSStr.PStor="Percent Storage";
+usbSStr.MExtr="Use Disk As External Root (Extroot)";
+usbSStr.FmtNow="Jetzt formatieren";
+usbSStr.ExtrS="Extroot";
+usbSStr.ExtrOff="Disable External Root";
+usbSStr.ExtDt="External root detected on";
+usbSStr.ExtrWarn="When using an external root drive or <strong>extroot</strong> an external USB drive becomes the root drive on your router. This can enable a very large root drive, but if you remove the drive all configuration changes since the extroot was activated will be lost.";
+
+//template
+usbSStr.Disk="Disk";
+usbSStr.SDir="Subdirectory";
+usbSStr.SNam="Share Name";
+usbSStr.SAppl="Settings Apply To";
+usbSStr.SPart="The Same Partition On Any USB Drive";
+usbSStr.SDriv="Only This Specific Drive";
+usbSStr.STyp="Share Type(s)";
+usbSStr.FAAcc="FTP/CIFS Anonymous Access";
+usbSStr.ANon="None";
+usbSStr.AROn="Read Only";
+usbSStr.ARWr="Read/Write";
+usbSStr.FAUsr="FTP/CIFS Users With Access";
+usbSStr.NAcc="NFS Access";
+usbSStr.NAccPo="NFS Access Policy";
+usbSStr.AnonAcc="Allow Anonymous Access";
+usbSStr.OnlyIPs="Allow Only The Following IPs";
+usbSStr.IPSub="Specify an IP or Subnet";
+usbSStr.FPath="FTP Path";
+usbSStr.NPath="NFS Path";
+
+//edit files
+usbSStr.EshDS="Edit Shared Disk";
+usbSStr.ChUPass="Ändere Benutzerpasswort";
+usbSStr.User="Benutzer";
+usbSStr.NPass="Neues Passwort";
+
+//javascript
+usbSStr.ICWErr="Invalid CIFS Workgroup";
+usbSStr.FWPErr="Cannot enable WAN FTP access because port conflicts with";
+usbSStr.FprErr="Invalid FTP passive port range";
+usbSStr.FprcErr="Passive FTP port range conflicts with";
+usbSStr.SSetErr="Einstellungen konnten nicht gespeichert werden";
+usbSStr.UsEmErr="Benutzername darf nicht leer sein";
+usbSStr.UsChErr="Benutzername darf nur aus Buchstaben und Zahlen bestehen";
+usbSStr.PwEmErr="Passwort darf nicht leer sein";
+usbSStr.PwEqErr="Passwörter sind nicht identisch";
+usbSStr.PwUErr="Passwort nicht geändert.";
+usbSStr.UsDErr="Doppelter Benutzername";
+usbSStr.Usnm="Benutzername";
+usbSStr.RsvErr="is reserved and not permitted";
+usbSStr.AUsErr="Benutzer konnte nicht hinzugefügt werden";
+usbSStr.AShErr="Freigabe konnte nicht hinzugefügt werden";
+usbSStr.NUsrErr="No Share Users Defined";
+usbSStr.NShUsrErr="No User To Add -- You must configure a new share user above.";
+usbSStr.NoShTypErr="You must select at least one share type (FTP, CIFS, NFS)";
+usbSStr.NoAAUsrErr="FTP and/or CIFS share type selected, but neither anonymous access or users are configured";
+usbSStr.DupDirErr="Specified Shared Directory Is Already Configured";
+usbSStr.DupShrErr="Share name is a duplicate";
+usbSStr.UpShrErr="Could not update share.";
+usbSStr.SwpPErr="ERROR: Invalid Allocation Percentages Specified";
+usbSStr.InvPErr="FEHLER: ungültiges Passwort";
+usbSStr.ChPass="Ändere Passwort";
+usbSStr.Name="Name";
+usbSStr.UDisk="Unmounting Disks";
+usbSStr.FDmsg1="Wollen Sie das Laufwerk wirklich formatieren";
+usbSStr.FDmsg2="Alle Daten auf dem Laufwerk gehen verloren.";
+usbSStr.FDmsg3="Um fortzufahren, geben Sie bitte das Router-Passwort ein";
+usbSStr.CfmPass1="Passwort akzeptiert.";
+usbSStr.CfmPass2="Dies ist Ihre LETZTE CHANCE abzubrechen. Drücken Sie 'OK' nur, wenn sie wirklich formatieren wollen";
+usbSStr.FmtMsg="Formatiere,\nBitte warten...";
+usbSStr.FmtCplt="Formatierung komplett.";
+usbSStr.ExtOffWarn="If you disable extroot, all settings and installed plugins may be lost. After disabling extroot, router requires a reboot. Continue?";
+usbSStr.ExtOffMsg="Disabling extroot...";
+usbSStr.SRboot="System wird neu gestartet";
