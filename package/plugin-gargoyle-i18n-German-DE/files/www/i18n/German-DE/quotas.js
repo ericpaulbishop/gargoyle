5aab4b532b3ac589212bdf47900d720631b7bbe2
Thomas Knepper <thknepper@gmx.de>
2014-02-06 14:46:15 +0100

    i18n/German: add quotas.js

---
+++ package/plugin-gargoyle-i18n-German-DE/files/www/i18n/German-DE/quotas.js
@@ -0,0 +1,69 @@
+﻿/*
+ * UTF-8 (with BOM) German-DE text strings for quotas.sh/quota_usage.sh html elements
+ */
+
+quotasStr.Section="Bandbreitenkontigente";
+quotasStr.AddQuota="Neues Kontigent hinzufügen";
+quotasStr.ActivQuotas="Aktive Kontigente";
+quotasStr.IPs="IP(s)";
+quotasStr.Active="Aktive";
+quotasStr.Limits="Begrenzungen";
+quotasStr.Totals="(Total/Down/Up)";
+quotasStr.OthersOne="Others (Individual)";
+quotasStr.OthersAll="Others (Combined)";
+quotasStr.Only="Only";
+quotasStr.AllExcept="All Times Except";
+quotasStr.AddError="Could not add quota.";
+quotasStr.LD1s="st";
+quotasStr.LD2s="nd";
+quotasStr.LD3s="rd";
+quotasStr.Digs="th";
+quotasStr.IPError="You must specify at least one valid IP or IP range";
+quotasStr.AllUnlimitedError="Upload, download and combined bandwidth limits cannot all be unlimited";
+quotasStr.DuplicateRange="Duplicate IP/Time Range -- only one quota per IP at a given time is allowed";
+quotasStr.OneTimeQuotaError="You may have only one quota at a given time for hosts without explicit quotas";
+quotasStr.OneNetworkQuotaError="You may have only one quota at a given time that applies to entire network";
+quotasStr.QuotaAddError="Could not add quota.";
+quotasStr.NA="NA"; //Not Applicable abbr
+
+//template
+quotasStr.AppliesTo="Gültigkeitsbereich";
+quotasStr.QLocalNet="Gesamtes lokales Netzwerk";
+quotasStr.QOnlyHosts="Nur für den/die folgenden Rechner";
+quotasStr.HostsWithoutQuotas="Alle Rechner ohne spezielle Kontigente";
+quotasStr.ComboHostsWithoutQuotas="Alle Rechner ohne spezielle Kontigente (kombiniert)";
+quotasStr.IPorRange="Specify an IP or IP range";
+quotasStr.MaxUp="Max Upload";
+quotasStr.MaxDown="Max Download";
+quotasStr.MaxUpDown="Max Total Up+Down";
+quotasStr.Unlimited="Unlimited";
+quotasStr.Limited="Limit to";
+quotasStr.QResets="Quota Resets";
+quotasStr.EvHour="Jede Stunde";
+quotasStr.EvDay="Jeden Tag";
+quotasStr.EvWeek="Jede Woche";
+quotasStr.EvMonth="Jeden Monat";
+quotasStr.ResetDay="Reset Day";
+quotasStr.ResetHour="Reset Hour";
+quotasStr.QuotaActive="Quota Is Active";
+quotasStr.TheseHours="These Hours";
+quotasStr.TheseDays="These Days";
+quotasStr.TheseDaysHours="These Days &amp; Hours";
+quotasStr.TheseTimes="These Weekly Times";
+quotasStr.Sample="e.g. Mon 00:30 - Thu 13:15, Fri 14:00 - Fri 15:00";
+quotasStr.Exceed="Wenn aufgebraucht";
+quotasStr.ShutdownNet="weiteren Internetzugriff deaktivieren";
+quotasStr.Throttle="Bandbreite begrenze";
+quotasStr.UpLimit="Upload Limit";
+quotasStr.DownLimit="Download Limit";
+quotasStr.UpClass="Upload Class";
+quotasStr.DownClass="Download Class";
+
+//quota_edit.sh
+quotasStr.ESection="Kontigente editieren";
+
+//quota_usage.sh/js
+quotasStr.USect="Bandwidth Quota Usage";
+quotasStr.All="All";
+quotasStr.Alws="Always";
+quotasStr.ColNms=["Host(s)", "Active", "% Total Used", "% Down Used", "% Up Used" ];
