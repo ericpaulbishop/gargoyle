84308d7a652da267ffd5de8db23f92446f30c3be
Thomas Knepper <thknepper@gmx.de>
2014-02-09 23:55:28 +0100

    i18n/German: update qos.js

--- package/plugin-gargoyle-i18n-German-DE/files/www/i18n/German-DE/qos.js
+++ package/plugin-gargoyle-i18n-German-DE/files/www/i18n/German-DE/qos.js
@@ -2,15 +2,15 @@
  * UTF-8 (with BOM) German-DE text strings for qos_(upload|download|edit_class|edit_rule).sh html elements
  */
 
-qosStr.URSection="QoS (Upload) -- Classification Rules";
-qosStr.DRSection="QoS (Download) -- Classification Rules";
-qosStr.UCSection="QoS (Upload) -- Service Classes";
-qosStr.DCSection="QoS (Download) -- Service Classes";
+qosStr.URSection="QoS (Upload) -- Klassifizierungsregeln";
+qosStr.DRSection="QoS (Download) -- Klassifizierungsregeln";
+qosStr.UCSection="QoS (Upload) -- Serviceklasse";
+qosStr.DCSection="QoS (Download) -- Serviceklasse";
 qosStr.DACCSect="QoS (Download) -- Active Congestion Control";
-qosStr.UEnable="Enable Quality of Service (Upload Direction)";
-qosStr.DEnable="Enable Quality of Service (Download Direction)";
-qosStr.UTotBand="Total (Upload) Bandwidth";
-qosStr.DTotBand="Total Download Bandwidth";
+qosStr.UEnable="Dienstequalität aktivieren (Upload)";
+qosStr.DEnable="Dienstequalität aktivieren (Download)";
+qosStr.UTotBand="Totale (Upload) Bandbreite";
+qosStr.DTotBand="Totale Download Bandbreite";
 
 qosStr.USCAbout="Each upload service class is specified by three parameters: percent bandwidth at capacity, minimum bandwidth and maximum bandwidth.";
 qosStr.DSCAbout="Each service class is specified by four parameters: percent bandwidth at capacity, realtime bandwidth and maximum bandwidth and the minimimze round trip time flag.";
@@ -23,30 +23,30 @@ qosStr.PerBandAboutD="<em>Percent bandwidth at capacity</em> is the percentage o
 qosStr.RTTAbout="<em>Minimize RTT</em> indicates to the active congestion controller that you wish to minimize round trip times (RTT) when this class is active. Use this setting for online gaming or VoIP applications that need low round trip times (ping times). Minimizing RTT comes at the expense of efficient WAN throughput so while these class are active your WAN throughput will decline (usually around 20%).";
 qosStr.MinSpeedWarn="If you are not using the ACC then you must establish what the minimum speed your ISP will deliver is and then set this number to that. In general ISPs do not provide a guaranteed minimum bandwith so it will take some experimentation and frustration on your part to arrive at a number. One approach is to start with a number which is half of what you think it should be and then test your link under full load and make sure everything works. Then increase it in steps, testing as you go until QoS starts to break down. You also may see that after your testing QoS works for a while and then stops working. This is because your ISP is getting overloaded due to demands from their other customers so they are no longer delivering to you the bandwidth they did during your testing. he solution, lower this number. Entering a number which is too high will result in QoS not meeting its class requirements. Entering a number which is too low will needlessly penalize your download speed. Because of all these complication I recommned you use the ACC when possible. Note that bandwidth is specified in kilobit/s. There are 8000 bits per kilobyte.";
 qosStr.QoSAbout="Quality of Service (QoS) provides a way to control how available bandwidth is allocated. Connections are classified into different &ldquo;service classes,&rdquo; each of which is allocated a share of the available bandwidth. QoS should be applied in cases where you want to divide available bandwidth between competing requirements. For example if you want your VoIP phone to work correctly while downloading videos. Another case would be if you want your bit torrents throttled back when you are web surfing.";
-qosStr.MaxBandAbout="<em>Maximale Bandbreite</em> gibt den absoluten maximalen Wert der Bandbreite in kbit/s an die diese Klasse nutzen kann. Auch wenn noch ungenutze Bandbreite verfügbar ist, wird dieser Service-Klasse nicht gestattet mehr als diese Bandbreite zu nutzen.";
+qosStr.MaxBandAbout="<em>Maximale Bandbreite</em> gibt den absoluten maximalen Wert der Bandbreite in kbit/s an die diese Klasse nutzen kann. Auch wenn noch ungenutze Bandbreite verfügbar ist, wird dieser Serviceklasse nicht gestattet mehr als diese Bandbreite zu nutzen.";
 qosStr.PackAbout="Packets are tested against the rules in the order specified -- rules toward the top have priority. As soon as a packet matches a rule it is classified, and the rest of the rules are ignored. The order of the rules can be altered using the arrow controls.";
-qosStr.DefServClassAbout="Die <em>Standard Service-Klasse</em> gibt an wie Packete auf die keine Regel zutrifft eingestuft werden sollen.";
+qosStr.DefServClassAbout="Die <em>Standard Serviceklasse</em> gibt an wie Packete auf die keine Regel zutrifft eingestuft werden sollen.";
 qosStr.AbACC="<p>The active congestion control (ACC) observes your download activity and automatically adjusts your download link limit to maintain proper QoS performance. ACC automatically compensates for changes in your ISP's download speed and the demand from your network adjusting the link speed to the highest speed possible which will maintain proper QoS function. The effective range of this control is between 15% and 100% of the total download bandwidth you entered above.</p><p>While ACC does not adjust your upload link speed you must enable and properly configure your upload QoS for it to function properly.</p><p><em>Ping Target-</em> The segment of network between your router and the ping target is where congestion is controlled. By monitoring the round trip ping times to the target congestion is detected. By default ACC uses your WAN gateway as the ping target. If you know that congestion on your link will occur in a different segment then you can enter an alternate ping target.</p><p><em>Manual Ping Limit-</em> Round trip ping times are compared against the ping limits. ACC controls the link limit to maintain ping times under the appropriate limit. By default Gargoyle attempts to automatically select appropriate target ping limits for you based on the link speeds you entered and the performance of your link it measures during initialization.  You cannot change the target ping time for the minRTT mode but by entering a manual time you can control the target ping time of the active mode.  The time you enter becomes the increase in the target ping time between minRTT and active mode. You can see the limits ACC is using in the [] brackets next to ping time limits field. </p>";
-qosStr.ServClass="Standard Service-Klasse";
+qosStr.ServClass="Standard Serviceklasse";
 
 qosStr.AddNewClassRule="Neue Classification Regel einfügen";
-qosStr.AddNewServiceRule="Neue Service Class einfügen";
-qosStr.SrcIP="Source IP";
-qosStr.SrcPort="Source Port(s)";
-qosStr.DstIP="Destination IP";
-qosStr.DstPort="Destination Port(s)";
-qosStr.MaxPktLen="Maximum Packet Length";
-qosStr.MinPktLen="Minimum Packet Length";
-qosStr.TrProto="Transport Protocol";
+qosStr.AddNewServiceRule="Neue Serviceklasse einfügen";
+qosStr.SrcIP="Quell-IP";
+qosStr.SrcPort="Quellport(s)";
+qosStr.DstIP="Ziel-IP";
+qosStr.DstPort="Zielport(s)";
+qosStr.MaxPktLen="Maximale Paketlänge";
+qosStr.MinPktLen="Minimale Paketlänge";
+qosStr.TrProto="Transport Protokoll";
 qosStr.Conreach="Connection bytes reach";
-qosStr.AppProto="Application (Layer7) Protocol";
+qosStr.AppProto="Anwendungsprotokoll (Layer7)";
 qosStr.SetClass="Set Service Class To";
-qosStr.SrvClassName="Service Class Name";
+qosStr.SrvClassName="Name der Serviceklasse";
 qosStr.PerBandCap="Percent Bandwidth At Capacity";
-qosStr.BandMin="Bandwidth Minimum";
-qosStr.BandMinNo="No Bandwidth Minimum";
-qosStr.BandMax="Bandwidth Maximum";
-qosStr.BandMaxNo="No Bandwidth Maximum";
+qosStr.BandMin="Minimale Bandbreite";
+qosStr.BandMinNo="Keine minimale Bandbreite";
+qosStr.BandMax="Maximale Bandbreite";
+qosStr.BandMaxNo="Keine maximale Bandbreite";
 qosStr.MinRTT="Minimize Round Trip Times (RTT)";
 qosStr.ActRTT="Minimize RTT (ping times) when active";
 qosStr.OptiWAN="Optimize WAN utilization";
@@ -69,21 +69,21 @@ qosStr.ACC_L_plim="The point at which the controller will act to maintain fairne
 qosStr.ACC_L_AC="Number of download classes with load over 4kbps.";
 
 //qos_edit_class.sh
-qosStr.QESrvClass="Edit QoS Service Class";
-qosStr.QESrvName="Service Class Name";
+qosStr.QESrvClass="Editiere QoS Serviceklasse";
+qosStr.QESrvName="Name der Serviceklasse";
 
 //qos_edit_rule.sh
-qosStr.QERulClass="Edit QoS Classification Rule";
+qosStr.QERulClass="Editiere QoS Klassifizierungsregeln";
 
 //javascript
 qosStr.MatchC="Match Criteria";
-qosStr.Classn="Classification";
-qosStr.Src="Source";
-qosStr.SrcP="Source Port";
-qosStr.Dst="Destination";
-qosStr.DstP="Destination Port";
+qosStr.Classn="Klassifizierung";
+qosStr.Src="Quelle";
+qosStr.SrcP="Quellport";
+qosStr.Dst="Ziel";
+qosStr.DstP="Zielport";
 qosStr.Connb="Connection bytes";
-qosStr.APro="Application Protocol";
+qosStr.APro="Anwendungsprotokoll";
 qosStr.pBdW="Percent BW";
 qosStr.mBdW="Min BW";
 qosStr.MBdW="Max BW";
