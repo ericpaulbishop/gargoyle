dbc6e9d40894c848c50125ce6d08d733bff877c1
Thomas Knepper <thknepper@gmx.de>
2014-02-18 13:25:50 +0100

    i18n/German: update about.js

--- package/plugin-gargoyle-i18n-German-DE/files/www/i18n/German-DE/about.js
+++ package/plugin-gargoyle-i18n-German-DE/files/www/i18n/German-DE/about.js
@@ -1,17 +1,16 @@
 ﻿/*
  * UTF-8 (with BOM) German-DE text strings for about.sh html elements
  */
-
 abt.CSect="Credits";
 abt.EBishop="Projekt Gründer, Hauptentwickler von Gargoyle";
-abt.PBixel="Implemented Active Congestion Control, as well as many other improvements to QoS";
-abt.AWronowski="Implemented Wake-on-LAN functionality along with several minor improvements/fixes";
-abt.CJackiewicz="Polish translation, several bugfixes, corrections, enhancements, plugins, custom Polish builds and Polish support via his website";
+abt.PBixel="Implementierte Active Congestion Control und viele weitere Verbesserungen im Bereich QoS";
+abt.AWronowski="Implementierte Wake-on-LAN, sowie einige kleinere Verbesserungen und Fehlerbehebungen";
+abt.CJackiewicz="Polnische Übersetzung, verschiedene Fehlerbehebungen, Berichtigungen und Verbesserungen, Plugins, Bereitstellung polnischer Gargoyle-Versionen und polnischer Support via seiner Webseite";
 abt.PKarbowski="Umsetzung zahlreicher Bugfixes und Performance-Verbesserungen";
-abt.TButler="Implemented several improvements for displaying tables and large buttons";
-abt.IFedorenko="Implemented inclusion of latest git commit in default version label";
-abt.BCoy="Provided several very helpful suggestions regarding the web design and CSS implementation of Gargoyle";
-abt.FRiC="Provided significant help in testing Gargoyle, particularly PPPoE functionality";
+abt.TButler="Implementierte einige Verbesserung zur Darstellung von Tabellen und großen Buttons";
+abt.IFedorenko="Implementierte das Hinzufügen des letzten GIT-Commit zur Version";
+abt.BCoy="Lieferte einige sehr hilfreiche Vorschläge bzgl. des Webdesigns und der CSS-Implementierung von Gargoyle";
+abt.FRiC="Signifikante Hilfe beim testen von Gargoyle, besonders der PPPoE Funktionalität";
 abt.logo="Das Gargoyle-Logo ist abgeleitet von der Gargoyle Schrift die von Manfred Klein erstellt wurde, und die er für kommerzielle und Nichtkommerzielle Nitzung freigegeben hat.";
 abt.openwrt="Die Gargoyle Schnittstelle ist eine grafische Benutzeroberfläche für die hervorragende OpenWrt Firmware. Dieses Projekt wäre ohne die harte Arbeit des OpenWRT-Teams nicht realisierbar.";
 abt.LSect="Lizenz";
