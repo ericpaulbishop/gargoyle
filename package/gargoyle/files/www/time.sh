1afccfc099e5320bc5f36c167f21ed116c24c879
Cezary Jackiewicz <cezary.jackiewicz@gmail.com>
2014-04-13 13:16:55 +0200

    gargoyle: current time from a common file.

--- package/gargoyle/files/www/time.sh
+++ package/gargoyle/files/www/time.sh
@@ -17,25 +17,8 @@
 	fi
 	echo "var timezoneData = parseTimezones(timezoneLines);"
 
-	dateformat=$(uci get gargoyle.global.dateformat 2>/dev/null)
-	if [ "$dateformat" == "iso" ]; then
-		current_time=$(date "+%Y/%m/%d %H:%M %Z")
-	elif [ "$dateformat" == "iso8601" ]; then
-		current_time=$(date "+%Y-%m-%d %H:%M %Z")
-	elif [ "$dateformat" == "australia" ]; then
-		current_time=$(date "+%d/%m/%y %H:%M %Z")
-	elif [ "$dateformat" == "russia" ]; then
-		current_time=$(date "+%d.%m.%Y %H:%M %Z")
-	elif [ "$dateformat" == "argentina" ]; then
-		current_time=$(date "+%d/%m/%Y %H:%M %Z")
-	else
-		current_time=$(date "+%D %H:%M %Z")
-	fi
-	timezone_is_utc=$(uci get system.@system[0].timezone | grep "^UTC" | sed 's/UTC//g')
-	if [ -n "$timezone_is_utc" ] ; then
-		current_time=$(echo $current_time | sed "s/UTC/UTC-$timezone_is_utc/g" | sed 's/\-\-/+/g')
-	fi
-	echo "var currentTime = \"$current_time\";"
+	. /usr/lib/gargoyle/current_time.sh
+
 	timeformat=$(uci -q get gargoyle.global.hour_style)
 	echo "var timeformat=$([[ -z $timeformat ]] && echo 0 || echo $timeformat);"
 %>
