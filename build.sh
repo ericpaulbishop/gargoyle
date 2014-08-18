d77252a3af4306f7972cd64eb26e21a49168107a
Eric <ericpaulbishop@gmail.com>
2014-08-18 11:25:45 -0400

    disable ANI by default

--- build.sh
+++ build.sh
@@ -46,7 +46,7 @@ set_version_variables()
 	# set svn revision number to use 
 	# you can set this to an alternate revision 
 	# or empty to checkout latest 
-	rnum=41503
+	rnum=42171
 
 	#set date here, so it's guaranteed the same for all images
 	#even though build can take several hours
