9a23528ae08ee57d1b67bbc687dcb49bf5f5be0d
Eric <ericpaulbishop@gmail.com>
2015-01-07 22:38:03 -0500

    fix display of second 5GHz band ssid on Status/Overview page of dual band routers

--- package/gargoyle/files/www/js/overview.js
+++ package/gargoyle/files/www/js/overview.js
@@ -147,7 +147,7 @@ function resetData()
 					var devBand = "G";
 					if(dev != "")
 					{
-						if(uciOriginal.get("wireless", dev, "hwmode") == "11na")
+						if(uciOriginal.get("wireless", dev, "hwmode") == "11na" || uciOriginal.get("wireless", dev, "hwmode") == "11a")
 						{
 							devBand = "A";
 						}
