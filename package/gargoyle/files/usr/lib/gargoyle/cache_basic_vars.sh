7d816bd7518611a1a28313d01aa7d07294ff79b4
Eric <ericpaulbishop@gmail.com>
2015-05-27 14:03:49 -0400

    Fix another bug with QMI pointed out by tanemann on the forum, devnames=>

--- package/gargoyle/files/usr/lib/gargoyle/cache_basic_vars.sh
+++ package/gargoyle/files/usr/lib/gargoyle/cache_basic_vars.sh
@@ -116,7 +116,7 @@ fi
 qmi_if=""
 if [ -e /sys/class/usbmisc/ ] ; then
 	local devnames=$(ls /sys/class/usbmisc)
-	for devname in devnames ; do
+	for devname in $devnames ; do
 		if [ -e "/sys/class/usbmisc/$devname/device/net" ] && [ -z "$qmi_if" ] ; then
 			qmi_if=$( ls "/sys/class/usbmisc/$devname/device/net" | head -n 1 )
 		fi
