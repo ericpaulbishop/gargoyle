92c7c5af95168c79618b1ed89d05baf903c394d6
Michael <lantis1008@users.noreply.github.com>
2015-06-29 22:13:04 +1000

    Update adblock.sh

--- package/plugin-gargoyle-adblock/files/www/adblock.sh
+++ package/plugin-gargoyle-adblock/files/www/adblock.sh
@@ -6,7 +6,7 @@
 	# itself remain covered by the GPL.
 	# See http://gargoyle-router.com/faq.html#qfoss for more information
 	eval $( gargoyle_session_validator -c "$COOKIE_hash" -e "$COOKIE_exp" -a "$HTTP_USER_AGENT" -i "$REMOTE_ADDR" -r "login.sh" -t $(uci get gargoyle.global.session_timeout) -b "$COOKIE_browser_time"  )
-	gargoyle_header_footer -h -s "system" -p "adblock" -c "internal.css" -j "adblock.js" -z "adblock.js" gargoyle adblock
+	gargoyle_header_footer -h -s "firewall" -p "adblock" -c "internal.css" -j "adblock.js" -z "adblock.js" gargoyle adblock
 %>
 
 
@@ -71,5 +71,5 @@
 </script>
 
 <%
-	gargoyle_header_footer -f -s "system" -p "adblock"
+	gargoyle_header_footer -f -s "firewall" -p "adblock"
 %>
