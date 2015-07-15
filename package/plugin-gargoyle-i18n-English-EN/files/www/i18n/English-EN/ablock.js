a9dba0a2e9761e3f728d8d95f8198231fe84a16d
Eric <ericpaulbishop@gmail.com>
2015-07-15 13:57:17 -0400

    if you are already using an adblocker in the browser elements and scripts named 'adblock' get blocked, so to make adblock plugin work more smoothly, rename adblock.sh and adblock.js to ablock.sh and ablock.js respectively, which should be a transparent change to the user

---
+++ package/plugin-gargoyle-i18n-English-EN/files/www/i18n/English-EN/ablock.js
@@ -0,0 +1,15 @@
+/*
+ * UTF-8 (with BOM) English-EN text strings for adblock.sh html elements
+ */
+
+ablock.Adblock="Adblock";
+ablock.ADBLOCKEn="Enable Adblock";
+ablock.ADBLOCKupdate="Update Blocklist";
+ablock.ADBLOCKLstrn="Last Run";
+ablock.ADBLOCKWireless="Only block ads for wireless clients";
+ablock.ADBLOCKTrans="Attempt to hide blocked ads";
+ablock.ADBLOCKHelp="This option attempts to replace the ads presented with a small transparent pixel. This can *sometimes* stop the ugly 'Page Not Found' errors that you see instead of ads.</p><p>However, with this option enabled, in some rare cases ads will show up as your routers login screen instead. This is not a phishing attempt, just a by-product of enabling this setting.";
+ablock.ADBLOCKExempts="IP range start";
+ablock.ADBLOCKExemptf="IP range finish";
+ablock.ADBLOCKExemptEn="Enable ad exemption for clients";
+ablock.ERRrange="Either the start or end of the exemption range is blank. If you only want to exempt 1 IP then use it as both the start and end of the range.";
