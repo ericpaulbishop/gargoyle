2492a1e95694b0c54e15418211c0fcaab434ad56
Eric <ericpaulbishop@gmail.com>
2015-09-24 13:51:16 -0400

    bump chaos calmer revision

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ set_version_variables()
 	# set svn revision number to use 
 	# you can set this to an alternate revision 
 	# or empty to checkout latest 
-	rnum=46419
+	rnum=47046
 
 	#set date here, so it's guaranteed the same for all images
 	#even though build can take several hours
