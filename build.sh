42ab6d458ed3633b9e7aed143c73f741dfda3db6
ispyisail <ispyisail@gmail.com>
2015-10-20 03:10:38 +1300

    Bump OpenWRT to 47233 CC

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ set_version_variables()
 	# set svn revision number to use 
 	# you can set this to an alternate revision 
 	# or empty to checkout latest 
-	rnum=47165
+	rnum=47233
 
 	#set date here, so it's guaranteed the same for all images
 	#even though build can take several hours
