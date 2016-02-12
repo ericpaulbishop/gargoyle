b6fde412cce235066784f2b705e67534f09c68bd
petbrau <peter.pb.braun@gmx.de>
2016-02-12 22:35:19 +0100

    Update ablock.js

--- package/plugin-gargoyle-i18n-German-DE/files/www/i18n/German-DE/ablock.js
+++ package/plugin-gargoyle-i18n-German-DE/files/www/i18n/German-DE/ablock.js
@@ -7,7 +7,7 @@ ablock.ADBLOCKEn="Aktiviere Adblock";
 ablock.ADBLOCKupdate="Aktualisiere Blockliste";
 ablock.ADBLOCKLstrn="Letzte Ausführung";
 ablock.ADBLOCKTrans="Versuchen, blockierte Anzeigen zu verbergen";
-ablock.ADBLOCKHelp="Diese Option versucht, die Werbung durch ein kleines transparentes Pixel zu ersetzen. Dies verhindert <i>manchmal</i> den 'Seite nicht gefunden'-Fehler, der anstelle der Werbung angezeigt wird.</p><p>Mit aktivierter Option, wird jedoch in einigen seltenen Fällen stattdessen die Login-Seite Ihres Routers angezeigt. Dies ist keine Phishing-Attacke, sondern lediglich ein Nebenprodukt der Aktivierung dieser Funktion.";
+ablock.ADBLOCKHelp="Diese Option versucht, die Werbung durch ein kleines transparentes Pixel zu ersetzen. Dies verhindert <i>in manchen Fällen</i> die Fehleranzeige 'Seite nicht gefunden', der statt der Werbung angezeigt wird.</p><p>Mit dieser Option, wird jedoch statdessen, in einigen seltenen Fällen, die Login-Seite Ihres Routers angezeigt. Dies ist keine Phishing-Attacke, sondern lediglich ein Nebenprodukt der Aktivierung dieser Funktion.";
 ablock.ADBLOCKExempts="IP-Bereich Anfang";
 ablock.ADBLOCKExemptf="IP-Bereich Ende";
 ablock.ADBLOCKExemptEn="Aktiviere Ausnahmen für IP-Bereich";
@@ -22,5 +22,5 @@ ablock.ADBLOCKCounter="Zeige";
 ablock.ADBLOCKSearchtoobig="Die Suche ergab mehr als 10000 Treffer. Es können nicht alle Ergebnisse angezeigt werden.";
 ablock.ADBLOCKWhitebig="Ihr weiße Liste verfügt über mehr als 10.000 Einträge. Die Liste kann nicht vollständig angezeigt werden.";
 ablock.ADBLOCKBlackbig="Ihr schwarze Liste verfügt über mehr als 10.000 Einträge. Die Liste kann nicht vollständig angezeigt werden.";
-ablock.ADBLOCKHelp2="In diesem Abschnitt können Sie Bereiche der Blockliste hinzufügen oder entfernen.</p><p>Sie können die Elemente der Blockliste, indem Sie sie in das Feld ein und klicken Sie auf Hinzufügen <i>Hinzufügen</i>. Sie können Elemente aus der Blockliste, indem Sie sie dann ihre Übertragung mit Hilfe der auf der weißen Liste zu entfernen <i>übertragen</i> Taste. Sie können Elemente aus der schwarze oder weiße Liste mit der Lösch <i>löschen</i> Schaltflächen.</p><p>Die Sperrliste können sehr groß sein, und das Display kann nur 10000 Artikel zeigen auf einmal. Sie können ein Element bei der Suche nach es, indem Sie in das Feld und mit dem zu finden <i>Suche</i> Taste.</p><p>Alle Boxen können Sie mehr als ein Element zu einem Zeitpunkt zu wählen.";
+ablock.ADBLOCKHelp2="In diesem Abschnitt können Sie Elemete der Blockliste hinzufügen oder entfernen.</p><p>Sie können die Elemente hinzufügen, indem Sie diese in das Feld eingeben und auf <i>Hinzufügen</i> klicken. Sie können Elemente aus der Blockliste entfernen, indem Sie diese auswählen und durch <i>übertragen</i> der weißen Liste hinzufügen. Sie können Elemente aus der schwarze oder weiße Liste mit der Schaltfäche <i>löschen</i> entfernen.</p><p>Die Listen können sehr groß sein, jedoch können nur 10000 Elemente angezeigt werden. Sie können ncoh einem Element suchen, indem Sie den Anzeigebereich anklicken und anschleißend <i>Suchen</i> verwenden.</p><p>Alle Anzeigebereiche erlauben die Auswahl von mehr als einem Element.";
 
