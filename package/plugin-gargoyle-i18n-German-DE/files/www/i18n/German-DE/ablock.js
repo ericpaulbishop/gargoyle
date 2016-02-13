c42d2fb645f96126a3ea424d538faaaeac760eda
petbrau <peter.pb.braun@gmx.de>
2016-02-13 11:43:45 +0100

    Update ablock.js

--- package/plugin-gargoyle-i18n-German-DE/files/www/i18n/German-DE/ablock.js
+++ package/plugin-gargoyle-i18n-German-DE/files/www/i18n/German-DE/ablock.js
@@ -14,7 +14,7 @@ ablock.ADBLOCKExemptEn="Ausnahmen aktivieren";
 ablock.ERRrange="Anfang oder das Ende des IP-Bereichs ist leer. Möchten Sie die Ausnahme nur für eine IP-Adresse festlegen, tragen Sie diese sowohl in das Anfangs-, als auch in das Ende-Feld, des Adressbereichs ein.";
 ablock.ADBLOCKBlocklist="Blockliste Suchergebnisse";
 ablock.ADBLOCKWhitelist="weiße Liste";
-ablock.ADBLOCKSearch="Suche nach Blockliste";
+ablock.ADBLOCKSearch="Suche in Blockliste";
 ablock.ADBLOCKBlacklist="schwarze Liste";
 ablock.ADBLOCKBlackdel="Löschen";
 ablock.ADBLOCKBlackadd="Hinzufügen";
@@ -22,5 +22,5 @@ ablock.ADBLOCKCounter="Zeige";
 ablock.ADBLOCKSearchtoobig="Die Suche ergab mehr als 10000 Treffer. Es können nicht alle Ergebnisse angezeigt werden.";
 ablock.ADBLOCKWhitebig="Ihr weiße Liste verfügt über mehr als 10.000 Einträge. Die Liste kann nicht vollständig angezeigt werden.";
 ablock.ADBLOCKBlackbig="Ihr schwarze Liste verfügt über mehr als 10.000 Einträge. Die Liste kann nicht vollständig angezeigt werden.";
-ablock.ADBLOCKHelp2="In diesem Abschnitt können Sie Elemete der Blockliste hinzufügen oder entfernen.</p><p>Sie können die Elemente hinzufügen, indem Sie diese in das Feld eingeben und auf <i>Hinzufügen</i> klicken. Sie können Elemente aus der Blockliste entfernen, indem Sie diese auswählen und durch <i>übertragen</i> der weißen Liste hinzufügen. Sie können Elemente aus der schwarze oder weiße Liste mit der Schaltfäche <i>löschen</i> entfernen.</p><p>Die Listen können sehr groß sein, jedoch können nur 10000 Elemente angezeigt werden. Sie können ncoh einem Element suchen, indem Sie den Anzeigebereich anklicken und anschleißend <i>Suchen</i> verwenden.</p><p>Alle Anzeigebereiche erlauben die Auswahl von mehr als einem Element.";
+ablock.ADBLOCKHelp2="In diesem Abschnitt können Sie Elemete der Blockliste hinzufügen oder entfernen.</p><p>Sie können die Elemente hinzufügen, indem Sie diese in das Eingabefeld eingeben und auf <i>Hinzufügen</i> klicken. Sie können Elemente aus der Blockliste entfernen, indem Sie diese markieren und durch drücken der kleinen Pfeil-Taste, der weißen Liste hinzufügen. Sie können Elemente aus der schwarzen oder weißen Liste mit der Schaltfäche <i>löschen</i> entfernen.</p><p>Die Listen können sehr groß sein, jedoch können nur 10000 Elemente angezeigt werden. Sie können nach einer blockierten Adresse suchen, indem Sie den Suchbegriff in das Leerfeld unterhalb Blockliste eingeben und auf <i>Suche in Blockliste</i> drücken.</p><p>Alle Anzeigebereiche erlauben die Auswahl von mehreren Elementen.";
 
