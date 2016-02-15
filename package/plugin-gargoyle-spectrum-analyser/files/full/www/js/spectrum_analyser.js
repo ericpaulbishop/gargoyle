d258e925d892da13a6f3ef5bffbfd3d324eeb0ab
Michael Gray <michael.gray@lantisproject.com>
2016-02-15 22:09:38 +1100

    Add Spectrum Analyser plugin (v1.0.0)

---
+++ package/plugin-gargoyle-spectrum-analyser/files/full/www/js/spectrum_analyser.js
@@ -0,0 +1,457 @@
+/*
+ * This program is copyright © 2015 Michael Gray and is distributed under the terms of the GNU GPL 
+ * version 2.0 with a special clarification/exception that permits adapting the program to
+ * configure proprietary "back end" software provided that all modifications to the web interface
+ * itself remain covered by the GPL.
+ * See http://gargoyle-router.com/faq.html#qfoss for more information
+ */
+var spectrum=new Object(); //part of i18n
+
+var interfaces;
+var freq_low;
+var freq_high;
+var detected;
+var band;
+
+// Xband = [[channel #],[centre freq],[low freq],[high freq]]
+var gband = [
+				[1,2,3,4,5,6,7,8,9,10,11,12,13,14],
+				[2.412,2.417,2.422,2.427,2.432,2.437,2.442,2.447,2.452,2.457,2.462,2.467,2.472,2.484],
+				[2.401,2.406,2.411,2.416,2.421,2.426,2.431,2.436,2.441,2.446,2.451,2.456,2.461,2.473],
+				[2.423,2.428,2.433,2.438,2.443,2.448,2.453,2.458,2.463,2.468,2.473,2.478,2.483,2.495]
+			]
+var aband = [
+				[36,40,44,48,52,56,60,64,100,104,108,112,116,120,124,128,132,136,140,149,153,157,161,165],
+				[5.180,5.200,5.220,5.240,5.260,5.280,5.300,5.320,5.500,5.520,5.540,5.560,5.580,5.600,5.620,5.640,5.660,5.680,5.700,5.745,5.765,5.785,5.805,5.825],
+				[5.170,5.190,5.210,5.230,5.250,5.270,5.290,5.310,5.490,5.510,5.530,5.550,5.570,5.590,5.610,5.630,5.650,5.670,5.690,5.735,5.755,5.775,5.795,5.815],
+				[5.190,5.210,5.230,5.250,5.270,5.290,5.310,5.330,5.510,5.530,5.550,5.570,5.590,5.610,5.630,5.650,5.670,5.690,5.710,5.755,5.775,5.795,5.815,5.835]
+			]
+
+function initialiseAll()
+{
+	var ivalues = [];
+	var inames = [];
+	//First, we should adjust the drop down list for the interfaces. We should identify which ones are 2.4 and 5ghz as well.
+	interfaces = parseInterfaces(wifiLines);
+	for(var x = 0; x < 2; x++)
+	{
+		ivalues.push(interfaces[x][0]);
+		inames.push(interfaces[x][1]);
+	}
+	setAllowableSelections('interface', ivalues, inames);	//populate the dropdown list
+
+	initialisePlots();
+
+	getWifiData();
+}
+
+
+function initialisePlots()
+{
+	//get the selected band, and then set the limits of the graph appropriately
+	band = interfaces[document.getElementById("interface").selectedIndex][1];
+	if(band == "2.4GHz")
+	{
+		freq_low = 2.4;		//technically 2.401, but 2.400 graphs better
+		freq_high = 2.5;		//technically 2.495, but 2.500 graphs better
+	}
+	else
+	{
+		freq_low = 5.165;	//technically 5.170, but 5.165 graphs better
+		freq_high = 5.840;	//technically 5.835, but 5.840 graphs better
+	}
+
+	//dummy code to setup a blank canvas before we populate any data
+	var spect = d3.select("#spectrum_plot"), 
+		WIDTH = 500, 
+		HEIGHT = 400, 
+		MARGINS = 
+		{ 
+			top: 20, 
+			right: 10, 
+			bottom: 50, 
+			left: 30
+		},
+		xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([freq_low,freq_high]),
+		yScale = d3.scale.linear().range([HEIGHT - MARGINS.bottom, MARGINS.top]).domain([-100,0]),
+		xAxis = d3.svg.axis().scale(xScale),
+		yAxis = d3.svg.axis().scale(yScale);
+
+	spect.selectAll(".axis").remove();	//kill the old axes
+	spect.selectAll("path").remove();	//kill the old lines
+	spect.selectAll("rect").remove();	//kill the old legend
+	spect.selectAll("text").remove();	//kill the old legend text
+}
+
+function changeBand()
+{
+	initialisePlots();
+	getWifiData();
+}
+
+function parseInterfaces(lines)
+{
+	//if we have no interfaces detected, exit
+	if(lines.length == 0) { return []; }
+
+	//otherwise, populate the data and assign correct frequency band
+	var interfaceData = [];
+	lineIndex = 0;
+
+	//lines is of the format:	wlanX ##; wlanY ##; etc etc.
+	for(lineIndex=0; lineIndex < lines.length; lineIndex++)
+	{
+		var nextLine = lines[lineIndex];
+		var wlan = nextLine.split(" ");
+
+		var interfaceid = wlan[0];
+		if(wlan[1] > 14)
+		{
+			var interfaceband = "5GHz";
+		}
+		else
+		{
+			var interfaceband = "2.4GHz";
+		}
+
+		interfaceData.push( [ interfaceid, interfaceband ] );
+	}
+	return interfaceData;
+}
+
+
+function getWifiData()
+{
+	var Commands = [];
+	var parsedWifiData = [];
+	var selectedband = interfaces[document.getElementById("interface").selectedIndex][0];
+
+	//scan command, substituting the correct WLAN
+	Commands.push("iw " + selectedband + " scan 2>&1 | awk -F'\\\n' '{print \"\\\"\"$0\"\\\"\" }'");
+
+	var param = getParameterDefinition("commands", Commands.join("\n")) + "&" + getParameterDefinition("hash", document.cookie.replace(/^.*hash=/,"").replace(/[\t ;]+.*$/, ""));	
+	var stateChangeFunction = function(req)
+	{
+		if (req.readyState == 4)
+		{
+			var shell_output = req.responseText.replace(/Success/, "");		//raw output from the shell
+			shell_output = shell_output.replace(/\"/g,"");					//remove any doubleq quotes
+			parsedWifiData = parseWifiData(shell_output);
+			if(parsedWifiData == -1)
+			{
+				alert(spectrum.Noscan);
+				plotall(parsedWifiData);
+			}
+			else
+			{
+				generateGraphData(parsedWifiData);
+			}
+		}
+	}
+	runAjax("POST", "utility/run_commands.sh", param, stateChangeFunction);
+}
+
+function parseWifiData(rawScanOutput)
+{
+	if((rawScanOutput != null) && (rawScanOutput.indexOf("\n") != 0) && (rawScanOutput.indexOf("\r") != 0))
+	{
+		var parsed = [ [],[],[],[],[] ];
+		var cells = rawScanOutput.split(/BSS [A-Fa-f0-9]{2}[:]/g);
+		cells.shift(); //get rid of anything before first AP data
+	
+		var getCellValues=function(id, cellLines)
+		{
+			var vals=[];
+			var lineIndex;
+			for(lineIndex=0; lineIndex < cellLines.length; lineIndex++)
+			{
+				var line = cellLines[lineIndex];
+				var idIndex = line.indexOf(id);
+				var cIndex  = line.indexOf(":");
+				var eqIndex = line.indexOf("=");
+				var splitIndex = cIndex;
+				if(splitIndex < 0 || (eqIndex >= 0 && eqIndex < splitIndex))
+				{
+					splitIndex = eqIndex;
+				}
+				if(idIndex >= 0 && splitIndex > idIndex)
+				{
+					var val=line.substr(splitIndex+1);
+					val = val.replace(/^[^\"]*\"/g, "");
+					val = val.replace(/\".*$/g, "");
+					val = val.replace(/^[ ]/g,"");
+					val = val.replace(/ dBm/g,"");
+					vals.push(val);
+				}
+			}
+			return vals;
+		}
+
+		while(cells.length > 0)
+		{
+			var cellData  = cells.shift();
+			var cellLines = cellData.split(/[\r\n]+/);
+
+			var ssid    = getCellValues("SSID", cellLines).shift();
+			var prichannel = getCellValues("primary channel", cellLines).shift();
+			var secchannel = getCellValues("secondary channel offset", cellLines).shift();
+			var sigStr = getCellValues("signal", cellLines).shift();
+			var vhtwidth = getCellValues("* channel width", cellLines).shift();
+
+			if(ssid != null && prichannel != null && secchannel != null && sigStr != null ) 
+			{
+				parsed[0].push(ssid);
+				parsed[1].push(prichannel);
+				parsed[2].push(secchannel);
+				parsed[3].push(sigStr);
+				parsed[4].push(vhtwidth);
+				//parsed[4].push( prichannel > 30 ? "5GHz" : "2.4GHz")	we don't need this anymore
+			}
+		}
+		//check for duplicate data and append _# if necessary
+		for(x = 0; x < parsed[0].length; x++)
+		{
+			append = 2;
+			for(y = (x+1); y < parsed[0].length; y++)
+			{
+				if((parsed[0][x] == parsed[0][y])/* && (parsed[4][x] == parsed[4][y])*/)	//second part only necessary if we end up scanning both spectrum at once
+				{
+					parsed[0][y] = parsed[0][y]+"_"+append;
+					append += 1;
+				}
+			}
+			if(append > 2)
+			{
+				parsed[0][x] = parsed[0][x]+"_1";
+			}
+		}
+		return parsed;
+	}
+	else
+	{
+		return(-1);
+	}
+}
+
+function generateGraphData(detected)
+{
+	var plotdata = [];
+	var freqoffset = 0.001;	//for making the graphs look pretty
+	var htoffset = 4;	//for 802.11n channels
+	var vhtoffset = 12;	//for 802.11ac channels
+	if(band == "2.4GHz")
+	{
+		for(x = 0; x < detected[0].length; x++)
+		{
+			var SSID = detected[0][x];
+			var channel = detected[1][x];
+			var secondary = detected[2][x];
+			var level = detected[3][x];
+			//var band = detected[4][x];
+			if(secondary == "no secondary")
+			{
+				plotdata[x*4] = {"ssid":SSID, "level":"-100", "freq":getfreqData(channel,2)};
+				plotdata[x*4+1] = {"ssid":SSID, "level":level, "freq":(getfreqData(channel,2)+freqoffset)};
+				plotdata[x*4+2] = {"ssid":SSID, "level":level, "freq":(getfreqData(channel,3)-freqoffset)};
+				plotdata[x*4+3] = {"ssid":SSID, "level":"-100", "freq":getfreqData(channel,3)};
+			}
+			if(secondary == "above")
+			{
+				plotdata[x*4] = {"ssid":SSID, "level":"-100", "freq":getfreqData(channel,2)};
+				plotdata[x*4+1] = {"ssid":SSID, "level":level, "freq":(getfreqData(channel,2)+freqoffset)};
+				plotdata[x*4+2] = {"ssid":SSID, "level":level, "freq":(getfreqData((channel-(-htoffset)),3)-freqoffset)};
+				plotdata[x*4+3] = {"ssid":SSID, "level":"-100", "freq":getfreqData((channel-(-htoffset)),3)};
+			}
+			if(secondary == "below")
+			{
+				plotdata[x*4] = {"ssid":SSID, "level":"-100", "freq":getfreqData((channel-htoffset),2)};
+				plotdata[x*4+1] = {"ssid":SSID, "level":level, "freq":(getfreqData((channel-htoffset),2)+freqoffset)};
+				plotdata[x*4+2] = {"ssid":SSID, "level":level, "freq":(getfreqData(channel,3)-freqoffset)};
+				plotdata[x*4+3] = {"ssid":SSID, "level":"-100", "freq":getfreqData(channel,3)};
+			}
+		}
+	}
+	else if(band == "5GHz")
+	{
+		for(x = 0; x < detected[0].length; x++)
+		{
+			var SSID = detected[0][x];
+			var channel = detected[1][x];
+			var secondary = detected[2][x];
+			var level = detected[3][x];
+			var vhtwidth = detected[4][x];
+			//var band = detected[4][x];
+			if(secondary == "no secondary")
+			{
+				plotdata[x*4] = {"ssid":SSID, "level":"-100", "freq":getfreqData(channel,2)};
+				plotdata[x*4+1] = {"ssid":SSID, "level":level, "freq":(getfreqData(channel,2)+freqoffset)};
+				plotdata[x*4+2] = {"ssid":SSID, "level":level, "freq":(getfreqData(channel,3)-freqoffset)};
+				plotdata[x*4+3] = {"ssid":SSID, "level":"-100", "freq":getfreqData(channel,3)};
+			}
+			if(secondary == "above")
+			{
+				if(! vhtwidth)
+				{
+					plotdata[x*4] = {"ssid":SSID, "level":"-100", "freq":getfreqData(channel,2)};
+					plotdata[x*4+1] = {"ssid":SSID, "level":level, "freq":(getfreqData(channel,2)+freqoffset)};
+					plotdata[x*4+2] = {"ssid":SSID, "level":level, "freq":(getfreqData((channel-(-htoffset)),3)-freqoffset)};
+					plotdata[x*4+3] = {"ssid":SSID, "level":"-100", "freq":getfreqData((channel-(-htoffset)),3)};
+				}
+				else		//when 160MHz channels come along, we should look for vhtwidth = 2, and then grab the 2 lots of "centre frequency"s data. for now this is fine.
+				{
+					plotdata[x*4] = {"ssid":SSID, "level":"-100", "freq":getfreqData(channel,2)};
+					plotdata[x*4+1] = {"ssid":SSID, "level":level, "freq":(getfreqData(channel,2)+freqoffset)};
+					plotdata[x*4+2] = {"ssid":SSID, "level":level, "freq":(getfreqData((channel-(-vhtoffset)),3)-freqoffset)};
+					plotdata[x*4+3] = {"ssid":SSID, "level":"-100", "freq":getfreqData((channel-(-vhtoffset)),3)};
+				}
+			}
+			if(secondary == "below")
+			{
+				plotdata[x*4] = {"ssid":SSID, "level":"-100", "freq":getfreqData((channel-htoffset),2)};
+				plotdata[x*4+1] = {"ssid":SSID, "level":level, "freq":(getfreqData((channel-htoffset),2)+freqoffset)};
+				plotdata[x*4+2] = {"ssid":SSID, "level":level, "freq":(getfreqData(channel,3)-freqoffset)};
+				plotdata[x*4+3] = {"ssid":SSID, "level":"-100", "freq":getfreqData(channel,3)};
+			}
+		}
+	}
+	else
+	{
+		//something is wroooooooooooooooooong
+		plotdata = -1;
+	}
+	
+	plotall(plotdata);
+}
+
+function getfreqData(channel,info)
+{
+	//info = 1, return centrefreq	info = 2, return lowfreq	info = 3, return highfreq
+	if(channel > 14)
+	{
+		a = aband[0].indexOf(parseInt(channel));
+		return aband[info][a];
+	}
+	else
+	{
+		a = gband[0].indexOf(parseInt(channel));
+		return gband[info][a];
+	}
+}
+
+function plotall(plotdata)
+{
+	var maxsig = -150;		//set the maximum signal level obsurdly low first
+	for (x = 0; x < plotdata.length; x++)		//then test for values higher than it until we find the highest
+	{
+		if(plotdata[x].level > maxsig)
+		{
+			maxsig = plotdata[x].level - (-10);	//set the maximum 10 above what it actually is so the graph doesn't look stupid
+		}
+	}
+	if(maxsig > 0)
+	{
+		maxsig = 0;			//if the max signal level got above 0, set it to 0
+	}
+	
+	var dataGroup = d3.nest()
+		.key(function(d) { return d.ssid; })
+		.entries(plotdata);				//break the data up into "keys" based on SSID which _should_ be unique (once i stuff around with them)
+	
+	var spect = d3.select("#spectrum_plot"), 
+		WIDTH = 500, 
+		HEIGHT = 400, 
+		MARGINS = 
+		{ 
+			top: 20, 
+			right: 10, 
+			bottom: 50, 
+			left: 30
+		},
+		lSpace = WIDTH/dataGroup.length;	//set aside some space for the legend
+		xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([freq_low,freq_high]),	//xscale width and range
+		yScale = d3.scale.linear().range([HEIGHT - MARGINS.bottom, MARGINS.top]).domain([-100,maxsig]),			//yscale height and range
+		xAxis = d3.svg.axis().scale(xScale),
+		yAxis = d3.svg.axis().scale(yScale);
+		
+	spect.selectAll(".axis").remove();	//kill the old axes
+	spect.selectAll("path").remove();	//kill the old lines
+	spect.selectAll("rect").remove();	//kill the old legend
+	spect.selectAll("text").remove();	//kill the old legend text
+	
+	xAxis = d3.svg.axis("xaxis")
+		.scale(xScale)
+		.tickValues(function(d) { if(band == "2.4GHz"){return gband[1];}else{return aband[1];} })
+		.tickFormat(function(d) { if(band == "2.4GHz"){a = gband[1].indexOf(d); return gband[0][a];}else{a = aband[1].indexOf(d); return aband[0][a];} }),
+  
+	yAxis = d3.svg.axis("yaxis")
+		.scale(yScale)
+		.orient("left");		//create the two axes
+		
+	if(band == "2.4GHz")
+	{
+		spect.append("svg:g")
+		.attr("class","axis")
+		.attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+		.call(xAxis);
+	}
+	else		//because channel numbers are bigger here, we need to rotate the labels or we run out of room.
+	{
+		spect.append("svg:g")
+		.attr("class","axis")
+		.attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+		.call(xAxis)
+		.selectAll("text")	
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", "-.3em")
+			.attr("transform", "rotate(-90)");
+	}
+	spect.append("svg:g")
+		.attr("class","axis")
+		.attr("transform", "translate(" + (MARGINS.left) + ",0)")
+		.call(yAxis);		//draw both axes
+		
+	var lineGen = d3.svg.line()
+		.x(function(d) { return xScale(d.freq); })
+		.y(function(d) { return yScale(d.level); });				//create a line generation function
+		
+	dataGroup.forEach(function(d, i) {
+		//randcolour = "hsl(" + Math.random() * 360 + ",100%,50%)";	//doesn't guarantee different colours but it is an easy implementation
+		randcolour = "hsl(" + 60 * i + ",100%,50%)";	//guarantees at least 6 different colours. then we repeat. looks better.
+		legendmarkersize = 10;
+		
+		spect.append('svg:path')
+			.attr('class', 'gline')
+			.attr('id', 'tag'+d.key.replace(/\s+/g, ''))	//unique id for each line based on the ssid
+			.attr('d', lineGen(d.values))
+			.attr('stroke', randcolour)
+			.attr('stroke-width', 2)
+			.attr('fill', 'none');			//draw each line
+		
+		spect.append('rect')
+			.attr('width', legendmarkersize)
+			.attr('height', legendmarkersize)
+			.attr("x", (lSpace / 2) + i * lSpace)		//evenly space little squares based on number of data points
+			.attr("y", HEIGHT-legendmarkersize)
+			.style('fill', randcolour)				//fill them the same colour as the line
+			.style('stroke', randcolour)
+			.on("mouseover", function() {
+				var existingcolour = d3.select("#tag"+d.key.replace(/\s+/g, ''))
+					.style("stroke");
+				d3.select("#tag"+d.key.replace(/\s+/g, ''))
+					.style("fill", existingcolour);
+			})
+			.on("mouseout", function() {
+                d3.select("#tag"+d.key.replace(/\s+/g, ''))
+					.style("fill", "none")
+					.style("opacity", 1);
+            });		//on mouseover, grab the colour of the line we are representing, and fill in the area below it. when we mouseout, set it back to normal
+			
+		spect.append("text")
+			.style("fill", "black")
+			.style("text-anchor", "end")
+			.attr("transform", "translate(" + ((lSpace / 2) + i * lSpace + (legendmarkersize / 2)) + "," + (HEIGHT+legendmarkersize) + ") rotate(-65)")
+			.text(d.key);	//write the text under the coloured rectangles. looks pretty. going to be a problem if someone uses an SSID of length 32 though.
+	});
+	
+}
---
+++ package/plugin-gargoyle-spectrum-analyser/files/full/www/spectrum_analyser.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/haserl
+<%
+	# This program is copyright © 2015 Michael Gray and is distributed under the terms of the GNU GPL 
+	# version 2.0 with a special clarification/exception that permits adapting the program to 
+	# configure proprietary "back end" software provided that all modifications to the web interface
+	# itself remain covered by the GPL.
+	# See http://gargoyle-router.com/faq.html#qfoss for more information
+	# I'd also like to note and thank Mike Bostock for his work on D3.js which is the underlying
+	# graphing library in use for this plugin.
+	eval $( gargoyle_session_validator -c "$COOKIE_hash" -e "$COOKIE_exp" -a "$HTTP_USER_AGENT" -i "$REMOTE_ADDR" -r "login.sh" -t $(uci get gargoyle.global.session_timeout) -b "$COOKIE_browser_time"  )
+	gargoyle_header_footer -h -s "system" -p "spectrum_analyser" -c "internal.css" -j "spectrum_analyser.js" -z "spectrum.js"
+%>
+
+<script>
+<%
+	#we need to find out which wireless interfaces exist.
+	echo "var wifiLines = new Array();"
+	aps=$( iwinfo | grep ESSID | awk ' { print $1 ; } ' )
+	for ap in $aps ; do
+		iwinfo $ap info | awk ' /^wlan/ { printf "wifiLines.push(\""$1" " ;} /Channel:/ {print ""$4"\");"}'
+	done
+%>
+</script>
+<!--add in graphing library-->
+<script src="js/d3.min.js" charset="utf-8"></script>
+
+<!--Extra CSS Styling-->
+<style>
+	.axis path {
+		fill: none;
+		stroke: #777;
+		shape-rendering: crispEdges;
+	}
+	.axis text {
+		font-family: Lato;
+		font-size: 13px;
+	}
+</style>
+
+<fieldset id="spectrum-fieldset">
+	<legend class="sectionheader"><%~ spectrum.Analyser %></legend>
+	
+	<select id="interface" onchange="changeBand()">
+		<option value="wlan0">wlan0 2.4GHz</option>
+		<option value="wlan1">wlan1 5GHz</option>
+	</select>
+	<br/>
+	<svg id="spectrum_plot" width="500" height="500"></svg>
+</fieldset>
+
+
+
+<script>
+<!--
+	initialiseAll();
+//-->
+</script>
+
+<%
+	gargoyle_header_footer -f -s "system" -p "spectrum_analyser"
+%>
