5bf9a3ac4707872a737cf0d0ccd24eb0811504f0
Michael Gray <michael.gray@lantisproject.com>
2016-10-27 21:45:02 +1100

    Bring Kernel to 3.18.44. DirtyCOW fix

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ set_version_variables()
 	# set precise commit in repo to use 
 	# you can set this to an alternate commit 
 	# or empty to checkout latest 
-	openwrt_commit="eadf19c0b43d2f75f196ea8d875a08c7c348530c"
+	openwrt_commit="1378f4a4d9908dd48dcf21ee526334d86344ec68"
 	openwrt_abbrev_commit=$( echo "$openwrt_commit" | cut -b 1-7 )
 	
 
