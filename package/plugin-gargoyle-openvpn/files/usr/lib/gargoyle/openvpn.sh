c1b191e3cd33ddcb1a55768fbc54d810ce662813
Michael Gray <michael.gray@lantisproject.com>
2017-12-19 21:05:51 +1100

    Implement verify-tls script for fast (temporary) revocation of certificates Everytime a client connects, the script is called which verifies if the CN exists in the list of allowed clients. If allowed, the connection succeeds, else the connection fails.

--- package/plugin-gargoyle-openvpn/files/usr/lib/gargoyle/openvpn.sh
+++ package/plugin-gargoyle-openvpn/files/usr/lib/gargoyle/openvpn.sh
@@ -209,6 +209,7 @@ tls-server
 ifconfig              $openvpn_server_internal_ip $openvpn_netmask
 topology              subnet
 client-config-dir     $OPENVPN_DIR/ccd
+verify-tls	      "/usr/lib/gargoyle/ovpn-cn-check.sh /etc/openvpn/verified-userlist"
 $openvpn_client_to_client
 
 $openvpn_duplicate_cn
@@ -523,6 +524,24 @@ remove_allowed_client()
 	rm -rf "$OPENVPN_DIR/ccd/$current_client"
 }
 
+remove_client_from_userlist()
+{
+	touch /etc/openvpn/verified-userlist
+	client_id="$1"
+	client_found=$(grep -wq "^$client_id" /etc/openvpn/verified-userlist)
+	if [ -z $client_found ] ; then
+		grep -wv "^$client_id" /etc/openvpn/verified-userlist > /etc/openvpn/vusrlst-temp
+		mv /etc/openvpn/vusrlst-temp /etc/openvpn/verified-userlist
+	fi
+}
+
+add_client_to_userlist()
+{
+	touch /etc/openvpn/verified-userlist
+	client_id="$1"
+	client_found=$(grep -wq "^$client_id" /etc/openvpn/verified-userlist)
+	[ -n $client_found ] && echo $client_id >> /etc/openvpn/verified-userlist
+}
 generate_test_configuration()
 {
 
@@ -569,8 +588,11 @@ regenerate_allowed_client_from_uci()
 	fi	
 	
 
-	if [ "$enabled" != "false" ] || [ "$enabled" != "0" ] ; then
+	if [ "$enabled" != "false" ] && [ "$enabled" != "0" ] ; then
 		create_allowed_client_conf "$id" "$remote" "$ip" "$subnet_ip" "$subnet_mask" "false" "$enabled"
+		add_client_to_userlist "$id"
+	else
+		remove_client_from_userlist "$id"
 	fi
 }
 
