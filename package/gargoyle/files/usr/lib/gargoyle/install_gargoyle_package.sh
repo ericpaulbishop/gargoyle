d196ae64e5e51fd63b0e506b10fdbc16e47b13e1
Michael Gray <michael.gray@lantisproject.com>
2018-03-25 17:42:14 +1100

    Fix plugin helper scripts not working

--- package/gargoyle/files/usr/lib/gargoyle/install_gargoyle_package.sh
+++ package/gargoyle/files/usr/lib/gargoyle/install_gargoyle_package.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-local plugin_root="/plugin_root" #doesn't change, we switch this to a symlink if we want to put files on USB drive
-local escaped_plugin_root=$(echo "$plugin_root" | sed 's/\//\\\//g' )
+plugin_root="/plugin_root" #doesn't change, we switch this to a symlink if we want to put files on USB drive
+escaped_plugin_root=$(echo "$plugin_root" | sed 's/\//\\\//g' )
 
 
-local pkg="$1"
+pkg="$1"
 if [ -z "$pkg" ] ; then
 	echo "ERROR: you must specify package to install"
 	echo "Usage: $0 [PACKAGE_NAME]"
@@ -15,14 +15,14 @@ fi
 ##############################################################
 # Prepare to install, create /plugin_root if it doesn't exist
 ##############################################################
-local have_plugin_root=$(cat /etc/opkg.conf | grep "dest.*$escaped_plugin_root" )
+have_plugin_root=$(cat /etc/opkg.conf | grep "dest.*$escaped_plugin_root" )
 if [ -z "$have_plugin_root" ] ; then
 	echo "dest plugin_root $plugin_root" >>/etc/opkg.conf
 fi
 if [ ! -e "$plugin_root" ] ; then  #tests if plugin_root exists, use -e since this may be directory or symlink
 	mkdir -p "$plugin_root"
 fi
-local link_dirs="etc tmp var"
+link_dirs="etc tmp var"
 for link_dir in $link_dirs ; do
 	if [ ! -h "$plugin_root/$link_dir" ] ; then
 		rm -rf "$plugin_root/$link_dir" 
