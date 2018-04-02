c5b1fabfb8df36218d62a06a75cb1c0a1ae6de7f
Michael Gray <michael.gray@lantisproject.com>
2018-04-02 21:56:52 +1000

    HTML error corrections

--- package/gargoyle/files/www/overview.sh
+++ package/gargoyle/files/www/overview.sh
@@ -131,7 +131,7 @@
 					<li class="list-group-item"><span class="list-group-item-title"><%~ WMAdd %>:</span><span id="wan_mac"></span></li>
 					<li class="list-group-item"><span class="list-group-item-title"><%~ WGtwy %>:</span><span id="wan_gateway"></span></li>
 					<li id="wan_dns_container" class="list-group-item"><span class="list-group-item-title"><%~ WDNS %>:</span><span id="wan_dns"></span></li>
-					<li id="wan_pppoe_container"class="list-group-item"><span class="list-group-item-title"><%~ WUptm %>:</span><span id="wan_pppoe_uptime"></span></li>
+					<li id="wan_pppoe_container" class="list-group-item"><span class="list-group-item-title"><%~ WUptm %>:</span><span id="wan_pppoe_uptime"></span></li>
 					<li id="wan_3g_container" class="list-group-item"><span class="list-group-item-title"><%~ W3GSS %>:</span><span id="wan_3g"></span></li>
 				</ul>
 			</div>
