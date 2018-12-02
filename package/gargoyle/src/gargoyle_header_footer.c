d4aa4bf65213d999af9dd3b65d9ff1dfbe480bb9
Michael Gray <michael.gray@lantisproject.com>
2018-12-02 20:05:34 +1100

    Add more detail to page titles

--- package/gargoyle/src/gargoyle_header_footer.c
+++ package/gargoyle/src/gargoyle_header_footer.c
@@ -220,6 +220,8 @@ int main(int argc, char **argv)
 		char* fallback_lang = "";
 		char* active_lang = "";
 		char* test_theme = "";
+		char* selected_section_page = "";
+		char* page_subtitle = "";
 		char** translation_strings = NULL;
 
 
@@ -304,13 +306,19 @@ int main(int argc, char **argv)
 		}
 #endif
 
+		selected_section_page = dynamic_strcat(3, selected_section, "_", selected_page);
+		if(get_uci_option(ctx, &e, p, "gargoyle", "display", selected_section_page) == UCI_OK)
+		{
+			page_subtitle = dynamic_strcat(2, " - ", get_option_value_string(uci_to_option(e)));
+		}
+
 		printf("<!DOCTYPE html>\n"
 			   "<head>\n"
 			   "\t<meta charset=\"utf-8\">\n"
 			   "\t<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n"
 			   "\t<meta name=\"description\" content=\"Gargoyle Firmware Webgui for router management.\">\n"
 			   "\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
-			   "\t<title>%s</title>\n", translation_strings == NULL ? title : translation_strings[0]);
+			   "\t<title>%s%s</title>\n", translation_strings == NULL ? title : translation_strings[0], page_subtitle);
 		test_theme = dynamic_strcat(5, web_root, theme_root, "/", theme, "/images/favicon.png");
 		//if the theme includes this file, use it, otherwise fallback to default gargoyle file
 		if (path_exists(test_theme) == PATH_IS_REGULAR_FILE || path_exists(test_theme) == PATH_IS_SYMLINK)
