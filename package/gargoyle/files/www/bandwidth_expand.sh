4cd72ebbf4a29cc6ad21427de9b762ce306f11db
Michael Gray <michael.gray@lantisproject.com>
2018-12-05 21:25:03 +1100

    Restore common css file to minimal header pages

--- package/gargoyle/files/www/bandwidth_expand.sh
+++ package/gargoyle/files/www/bandwidth_expand.sh
@@ -6,7 +6,7 @@
 	# itself remain covered by the GPL.
 	# See http://gargoyle-router.com/faq.html#qfoss for more information
 	eval $( gargoyle_session_validator -c "$COOKIE_hash" -e "$COOKIE_exp" -a "$HTTP_USER_AGENT" -i "$REMOTE_ADDR" -r "login.sh" -t $(uci get gargoyle.global.session_timeout) -b "$COOKIE_browser_time"  )
-	gargoyle_header_footer -m
+	gargoyle_header_footer -m -c "common.css"
 %>
 	<strong><span id="plot_title" class="col-xs-12"></span></strong>
 	<label class="col-xs-4" id="plot1_title"><%~ Plot %> 1</label>
