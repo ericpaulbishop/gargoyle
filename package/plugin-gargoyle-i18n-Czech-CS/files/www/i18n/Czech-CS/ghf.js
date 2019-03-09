827e72aa04903f7c6144257d307931bf36b8d187
RomanHK <48184158+RomanHK@users.noreply.github.com>
2019-03-09 20:20:20 +0100

    Update ghf.js

--- package/plugin-gargoyle-i18n-Czech-CS/files/www/i18n/Czech-CS/ghf.js
+++ package/plugin-gargoyle-i18n-Czech-CS/files/www/i18n/Czech-CS/ghf.js
@@ -2,7 +2,7 @@
  * UTF-8 (with BOM) Czech-CS strings for gargoyle_header_footer  html elements
  */
 
-ghf.title="Nástroj řízení routeru Gargoyle";
-ghf.desc="Nástroj řízení routeru";
+ghf.title="Nástroj řízení směrovače Gargoyle";
+ghf.desc="Nástroj řízení směrovače";
 ghf.devn="Název zařízení";
 ghf.waits="Prosím čekejte pokud se aplikují nastavení";
