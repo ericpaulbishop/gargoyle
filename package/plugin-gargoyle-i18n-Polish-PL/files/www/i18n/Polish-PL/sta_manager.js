18a80b0fff1d30762f244cc703ee8d6473954f2a
Cezary Jackiewicz <cezary@eko.one.pl>
2019-10-29 18:27:15 +0100

    i18n/Polish: add missing translation, add STA Manager

---
+++ package/plugin-gargoyle-i18n-Polish-PL/files/www/i18n/Polish-PL/sta_manager.js
@@ -0,0 +1,34 @@
+﻿/*
+ * UTF-8 (with BOM) Polish-PL text strings for sta_manager.sh html elements
+ */
+
+stamgrStr.mStaMgr="Menedżer połączeń bezprzewodowych";
+stamgrStr.StaMgrSettings="Ustawienia menedżera połączeń";
+stamgrStr.StaMgrEn="Włącz Menedżer połączeń bezprzewodowych";
+stamgrStr.MaxConnAttempts="Maks. liczba prób połączenia";
+stamgrStr.MaxWait="Maks. czas oczekiwania pomiędzy próbami połączenia";
+stamgrStr.DisconnectQualThresh="Próg jakości rozłączenia";
+stamgrStr.ConnectQualThresh="Próg jakości połączenia";
+stamgrStr.BlacklistTimer="Zabraniaj błędnych połączeń przez";
+stamgrStr.BlacklistTime="Pozostały czas";
+stamgrStr.Status="Status";
+stamgrStr.WirelessAPs="Bezprzewodowe punkty dostępowe";
+stamgrStr.AddAP="Dodawanie AP";
+stamgrStr.EAP="Edycja AP";
+stamgrStr.LastUpdate="Ostatnia aktualizacja";
+stamgrStr.CurrAP="Bieżący AP";
+stamgrStr.Connected="Połączony";
+stamgrStr.BlacklistAP="Zabronione AP";
+stamgrStr.NoStacfgErr="Sieć Wi-Fi musi być ustawiona jako klient lub klient+AP aby móc używać Menedżera połączeń";
+
+//Javascript
+stamgrStr.cfgid="ID konfiguracji";
+stamgrStr.Band="Pasmo";
+stamgrStr.Encryption="Szyfrowanie";
+stamgrStr.Password="Hasło";
+stamgrStr.AErr="Nie można dodać wiersza.";
+stamgrStr.upErr="Nie można zaktualizować wiersza.";
+stamgrStr.discontoohighErr="Jakość rozłączenia musi być poniżej 90%";
+stamgrStr.contoolowErr="Jakość rozłączenia musi być powyżej 30%";
+stamgrStr.discongtconErr="Jakość rozłączenia musi być poniżej jakości połączenia";
+
