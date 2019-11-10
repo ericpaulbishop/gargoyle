328216514bee3567f62436c207611e1a9f10545a
Michael Gray <michael.gray@lantisproject.com>
2019-11-10 16:17:55 +1100

    Bump to Openwrt 18.06.5 Fix kernel download

--- netfilter-match-modules/integrate_netfilter_modules.sh
+++ netfilter-match-modules/integrate_netfilter_modules.sh
@@ -148,7 +148,7 @@ ifeq ($(call qstrip,$(CONFIG_EXTERNAL_KERNEL_TREE))$(call qstrip,$(CONFIG_KERNEL
   ifeq ($(word 1,$(subst ., ,$(KERNEL_BASE))),3)
     LINUX_SITE:=@KERNEL/linux/kernel/v3.x$(TESTING)
   else
-    LINUX_SITE:=@KERNEL/linux/kernel/v$(KERNEL)$(TESTING)
+    LINUX_SITE:=@KERNEL/linux/kernel/v$(word 1,$(subst ., ,$(KERNEL_BASE))).x$(TESTING)
   endif
 endif
 
