c962a2acf0e0ff2e97b8a6af122f5aa0f680613b
Cezary Jackiewicz <cezary@eko.one.pl>
2020-07-05 15:47:53 +0200

    i18n/Polish: add missing translation

--- package/plugin-gargoyle-i18n-Polish-PL/files/www/i18n/Polish-PL/strings.js
+++ package/plugin-gargoyle-i18n-Polish-PL/files/www/i18n/Polish-PL/strings.js
@@ -124,7 +124,7 @@ UI.conn="połączony";
 UI.YES="Tak";
 UI.NO="Nie";
 UI.optional="opcja";
-UI.IPFam="IP Family";
+UI.IPFam="Rodzina IP";
 
 UI.AJAX="Wymagane wsparcie AJAX ze strony przeglądarki";
 UI.AJAXUpg="Niezbędne jest użycie przeglądarki ze wsparciem dla AJAX. Może to być jedna z wymienionych: Firefox 2.0+, Safari lub IE 6+.";
