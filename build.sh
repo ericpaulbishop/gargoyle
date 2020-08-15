58bc557648c7f9a7914dee706767bf57d9fe1531
Michael Gray <michael.gray@lantisproject.com>
2020-08-15 19:55:35 +1000

    Bump OpenWrt to latest 19.07

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ set_version_variables()
 	# set precise commit in repo to use 
 	# you can set this to an alternate commit 
 	# or empty to checkout latest
-	openwrt_commit="f3f38f40da74a193b4177be8d0fa746d00a753ea"
+	openwrt_commit="5af8da37870dc05dbe2e57e04be714b80f4aa21d"
 	openwrt_abbrev_commit=$( echo "$openwrt_commit" | cut -b 1-7 )
 	
 
