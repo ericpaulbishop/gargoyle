19a067027a8b8f713c2a69793229f1d19d85552e
Michael Gray <michael.gray@lantisproject.com>
2021-10-01 14:46:20 +1000

    gargoyle: Fix wifi signal dBm display for some devices e.g. Archer C7

--- package/gargoyle/files/www/js/hosts.js
+++ package/gargoyle/files/www/js/hosts.js
@@ -318,12 +318,16 @@ function parseWifi(arpHash, wirelessDriver, lines, apsta)
 		var toHexTwo = function(num) { var ret = parseInt(num).toString(16).toUpperCase(); ret= ret.length < 2 ? "0" + ret : ret.substr(0,2); return ret; }
 
 		var sig = parseInt(mbs[2]);
+		// Some drivers pass through quality rather than signal level. Convert this back. Also make sure it fits within the standard max/min range
+		sig = sig > 0 ? sig - 110 : sig;
+		sig = sig > -40 ? -40 : sig;
+		sig = sig < -110 ? -110 : sig;
 		var color = sig < -80  ? "#AA0000" : "";
 		color = sig >= -80 && sig < -70 ? "#AA" + toHexTwo(170*((sig+80)/10.0)) + "00" : color;
 		color = sig >= -70 && sig < -60 ? "#" + toHexTwo(170-(170*(sig+70)/10.0)) + "AA00" : color;
 		color = sig >= -60 ? "#00AA00" : color;
 		var sigSpan = document.createElement("span");
-		sigSpan.appendChild(document.createTextNode(mbs[2] + " dBm"));
+		sigSpan.appendChild(document.createTextNode(sig + " dBm"));
 		sigSpan.style.color = color;
 		mbs[2] = sigSpan;
 
