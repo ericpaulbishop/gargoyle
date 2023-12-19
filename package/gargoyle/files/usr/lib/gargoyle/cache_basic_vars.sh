0c20c947466960f9746654dbd8a5a57376d3d186
Michael Gray <michael.gray@lantisproject.com>
2023-12-19 21:14:19 +1100

    gargoyle: Changes to support wl phy naming - Needs testing, may not be consistent across devices - Have left phy and wl in place where possible

--- package/gargoyle/files/usr/lib/gargoyle/cache_basic_vars.sh
+++ package/gargoyle/files/usr/lib/gargoyle/cache_basic_vars.sh
@@ -11,15 +11,17 @@ print_mac80211_channels_for_wifi_dev()
 	echo "nextChFreq = [];" >> "$out"
 	echo "nextChPwr  = [];" >> "$out"
 	mode=$(uci get wireless.$wifi_dev.band)
-	phyname=$(iwinfo nl80211 phyname $wifi_dev)
+	phyname=$(iwinfo nl80211 phyname $wifi_dev 2>&1)
 	[ "$phyname" = "Phy not found" ] && phyname="phy$dev_num"
+	# Check for wl naming
+	[ "$(iwinfo $phyname h 2>&1)" = "No such wireless device: $phyname" ] && phyname="wl$dev_num"
 
 	htpat="\bHT[0-9]\{2,3\}"
 	vhtpat="\bVHT[0-9]\{2,3\}\(+[0-9]\{2\}\)\?"
 	hepat="\bHE[0-9]\{2,3\}\(+[0-9]\{2\}\)\?"
-	wifiN=$(iwinfo $wifi_dev h | sed -e "s/$vhtpat//g" -e "s/$hepat//g" -e 's/^[ ]*//')
-	wifiAC=$(iwinfo $wifi_dev h | sed -e "s/$htpat//g" -e "s/$hepat//g" -e 's/^[ ]*//')
-	wifiAX=$(iwinfo $wifi_dev h | sed -e "s/$htpat//g" -e "s/$vhtpat//g" -e 's/^[ ]*//')
+	wifiN=$(iwinfo $phyname h | sed -e "s/$vhtpat//g" -e "s/$hepat//g" -e 's/^[ ]*//')
+	wifiAC=$(iwinfo $phyname h | sed -e "s/$htpat//g" -e "s/$hepat//g" -e 's/^[ ]*//')
+	wifiAX=$(iwinfo $phyname h | sed -e "s/$htpat//g" -e "s/$vhtpat//g" -e 's/^[ ]*//')
 	if [ "$wifiAC" ] ; then
 		maxAC=$(echo $wifiAC | awk -F " VHT" '{print $NF}')
 		AC80P80=$(echo $wifiAC | grep "VHT80+80")
@@ -151,7 +153,7 @@ if [ -e /lib/wifi/broadcom.sh ] ; then
 	echo "var AwifiAC = false;" >> "$out_file"
 	echo "var AwifiAX = false;" >> "$out_file"
 	echo "var dualBandWireless=false;" >> "$out_file"
-elif [ -e /lib/wifi/mac80211.sh ] && [ -e "/sys/class/ieee80211/phy0" ] ; then
+elif [ -e /lib/wifi/mac80211.sh ] && [ -e "/sys/class/ieee80211/phy0" -o -e "/sys/class/ieee80211/wl0" ] ; then
 	echo 'var wirelessDriver="mac80211";' >> "$out_file"
 	echo 'var mac80211Channels = [];' >> "$out_file"
 	echo 'var mac80211ChFreqs = [];' >> "$out_file"
@@ -160,7 +162,7 @@ elif [ -e /lib/wifi/mac80211.sh ] && [ -e "/sys/class/ieee80211/phy0" ] ; then
 	echo "var nextCh=[];" >> "$out_file"
 	
 	#test for dual band
-	if [ "$(uci show wireless | grep wifi-device | wc -l)" = "2" ] && [ -e "/sys/class/ieee80211/phy1" ] && [ ! "$(uci get wireless.@wifi-device[0].band)" = "$(uci get wireless.@wifi-device[1].band)"  ] ; then
+	if [ "$(uci show wireless | grep wifi-device | wc -l)" = "2" ] && [ -e "/sys/class/ieee80211/phy1" -o -e "/sys/class/ieee80211/wl1" ] && [ ! "$(uci get wireless.@wifi-device[0].band)" = "$(uci get wireless.@wifi-device[1].band)"  ] ; then
 		echo "var dualBandWireless=true;" >> "$out_file"
 		dualband='true'
 	else
